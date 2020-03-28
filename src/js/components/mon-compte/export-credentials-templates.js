344907090fee36464ef1e53767d8ce1d20893f1a
jfeudeline <jfeudeline@gmail.com>
2020-03-28 19:00:20 +0100

    Real R script

--- src/js/components/mon-compte/export-credentials-templates.js
+++ src/js/components/mon-compte/export-credentials-templates.js
@@ -1,19 +1,26 @@
 const exportTypes = [
 	{
-		id: 'python',
-		label: 'Python',
-		extension: 'py',
-		text: (c) =>
-			`le texte python :
-access key id: ${c.AWS_ACCESS_KEY_ID}
-s3 endpoint: ${c.AWS_S3_ENDPOINT}`,
-	},
-	{
 		id: 'r',
 		label: 'R',
 		extension: 'R',
 		text: (c) =>
-			`le texte R :
+			`install.packages("aws.s3", repos = "https://cloud.R-project.org")
+
+Sys.setenv("AWS_ACCESS_KEY_ID" = ${c.AWS_ACCESS_KEY_ID},
+           "AWS_SECRET_ACCESS_KEY" = ${c.AWS_SECRET_ACCESS_KEY},
+           "AWS_DEFAULT_REGION" = ${c.AWS_DEFAULT_REGION},
+           "AWS_SESSION_TOKEN" = ${c.AWS_SESSION_TOKEN},
+           "AWS_S3_ENDPOINT"= ${c.AWS_S3_ENDPOINT})
+
+library("aws.s3")
+bucketlist()`,
+	},
+	{
+		id: 'python',
+		label: 'Python (fake)',
+		extension: 'py',
+		text: (c) =>
+			`le texte python :
 access key id: ${c.AWS_ACCESS_KEY_ID}
 s3 endpoint: ${c.AWS_S3_ENDPOINT}`,
 	},
