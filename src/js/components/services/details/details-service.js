a746d5f24d1fe70e1ee2d278aea75a946caba60f
Nicolas Laval <nico.laval28@gmail.com>
2020-03-31 11:31:24 +0200

    Fix #34 (#46)

--- src/js/components/services/details/details-service.js
+++ src/js/components/services/details/details-service.js
@@ -4,9 +4,9 @@ import {
 	RocketChatIcon,
 	GithubIcon,
 } from 'js/components/commons/icons';
-import { Typography, Paper, Fab, Icon } from '@material-ui/core';
-// import { getServiceAvatar } from "js/components/commons/service-liste";
+import { Typography, Paper, Fab } from '@material-ui/core';
 import FilDAriane, { fil } from 'js/components/commons/fil-d-ariane';
+import CopyableField from 'js/components/commons/copyable-field';
 import { axiosURL, wrapPromise } from 'js/utils';
 import conf from 'js/configuration';
 
@@ -15,20 +15,21 @@ const resource = wrapPromise(axiosURL(conf.API.SERVICES_URL));
 const DetailsService = () => {
 	const { services } = resource.read();
 	const serviceId = window.location.pathname.replace('/services/', '');
-	// TODO: replace title by id
-	const service = services.find((s) => s.title === serviceId) || {};
+	const service = services.find((s) => s.id === serviceId) || {};
 
 	const {
 		urls,
 		channel,
-		monitoring,
+		dashboard,
 		title,
 		subtitle,
 		logo,
 		description,
 		caution,
-		contribute,
+		scm,
 	} = service;
+
+	const toolbar = dashboard || channel || scm;
 
 	return (
 		<>
@@ -55,53 +56,40 @@ const DetailsService = () => {
 				<div className="logo-service">
 					<img src={logo} alt="logo" />
 				</div>
-				<Paper className="onyxia-toolbar" elevation={1}>
-					{monitoring ? (
-						<Fab
-							className="bouton"
-							color="primary"
-							title="grafana"
-							onClick={() => window.open(monitoring)}
-						>
-							<GrafanaIcon />
-						</Fab>
-					) : null}
-					{channel ? (
-						<Fab
-							className="bouton"
-							color="primary"
-							title="rocketchat"
-							onClick={() => window.open(channel)}
-						>
-							<RocketChatIcon />
-						</Fab>
-					) : null}
-					{contribute ? (
-						<Fab
-							className="bouton"
-							color="primary"
-							title="contribuer"
-							onClick={() => window.open(contribute)}
-						>
-							<GithubIcon width={20} height={20} />
-						</Fab>
-					) : null}
-					{urls ? (
-						<>
-							{urls.map(({ name, url }) => (
-								<Fab
-									key={`service-${name}`}
-									className="bouton"
-									color="secondary"
-									title={`service-${name}`}
-									onClick={() => window.open(url)}
-								>
-									<Icon>open_in_new</Icon>
-								</Fab>
-							))}
-						</>
-					) : null}
-				</Paper>
+				{toolbar && (
+					<Paper className="onyxia-toolbar" elevation={1}>
+						{dashboard ? (
+							<Fab
+								className="bouton"
+								color="primary"
+								title="dashboard"
+								onClick={() => window.open(dashboard)}
+							>
+								<GrafanaIcon />
+							</Fab>
+						) : null}
+						{channel ? (
+							<Fab
+								className="bouton"
+								color="primary"
+								title="rocketchat"
+								onClick={() => window.open(channel)}
+							>
+								<RocketChatIcon />
+							</Fab>
+						) : null}
+						{scm ? (
+							<Fab
+								className="bouton"
+								color="primary"
+								title="contribuer"
+								onClick={() => window.open(scm)}
+							>
+								<GithubIcon width={20} height={20} />
+							</Fab>
+						) : null}
+					</Paper>
+				)}
 				{description && (
 					<Paper className="paper" elevation={1}>
 						<Typography variant="h3" gutterBottom>
@@ -122,6 +110,22 @@ const DetailsService = () => {
 						</Typography>
 					</Paper>
 				) : null}
+				{urls.length > 0 && (
+					<Paper className="paper" elevation={1}>
+						<Typography variant="h3" align="left">
+							Services associés
+						</Typography>
+						{urls.map(({ name, url, description }) => (
+							<CopyableField
+								key={url}
+								copy
+								label={name}
+								description={description}
+								value={url || ''}
+							/>
+						))}
+					</Paper>
+				)}
 			</div>
 		</>
 	);
