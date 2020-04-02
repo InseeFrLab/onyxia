a8ee452d30c070b2adb8a9154f42f7bfaeb72031
NicoLaval <nicolas.laval@insee.fr>
2020-04-03 00:38:24 +0200

    Update component.js

--- src/js/components/mes-secrets/home/component.js
+++ src/js/components/mes-secrets/home/component.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
+import Loader from 'js/components/commons/loader';
 
-const SecretHome = ({ idep }) => {
-	return <Redirect to={`/mes-secrets/${idep}`} />;
-};
+const SecretHome = ({ idep }) =>
+	idep ? <Redirect to={`mes-secrets/${idep}`} /> : <Loader em={18} />;
 
 export default SecretHome;
