785ad6a1ce129506c3f8618e42559e04c884d580
Olivier Levitt <ungawa14@gmail.com>
2020-04-02 23:01:10 +0200

    Fix #56 : add a beta test toggle

--- src/js/components/my-lab/catalogue/catalogue-navigation/root/catalogues.js
+++ src/js/components/my-lab/catalogue/catalogue-navigation/root/catalogues.js
@@ -3,16 +3,18 @@ import Grid from '@material-ui/core/Grid';
 import Catalogue from '../catalogue.component';
 import { axiosPublic, wrapPromise } from 'js/utils';
 import api from 'js/redux/api';
+import { hasOptedInForBetaTest } from '../../../../../configuration/betatest';
 
 const resource = wrapPromise(axiosPublic(api.catalogue));
 
 const Catalogues = () => {
 	const { universes } = resource.read();
+	const betaTester = hasOptedInForBetaTest();
 	return (
 		<div className="contenu catalogue">
 			<Grid container spacing={2}>
 				{universes
-					.filter((catalogue) => catalogue.status === 'PROD')
+					.filter((catalogue) => catalogue.status === 'PROD' || betaTester)
 					.map((catalogue) => (
 						<Catalogue catalogue={catalogue} key={catalogue.id} />
 					))}
