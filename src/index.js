14f3b79d050d709d0ea3b3ba12595d564c0f84cf
NicoLaval <nicolas.laval@insee.fr>
2020-04-02 23:33:06 +0200

    Up

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+import axios from 'axios';
 import App from 'js/components';
 import { store } from 'js/redux';
 import { getKeycloak } from 'js/utils';
@@ -9,6 +10,11 @@ import { gelLocalToken, setLocalToken } from 'js/utils';
 import JavascriptTimeAgo from 'javascript-time-ago';
 import fr from 'javascript-time-ago/locale/fr';
 import configuration from 'js/configuration';
+import { getVaultToken } from 'js/vault-client';
+import conf from 'js/configuration';
+import generator from 'generate-password';
+
+const VAULT_BASE_URI = conf.VAULT.VAULT_BASE_URI;
 
 JavascriptTimeAgo.locale(fr);
 
@@ -38,6 +44,32 @@ const initialiseKeycloak = () =>
 							idToken: getKeycloak().idToken,
 						})
 					);
+					const idep = getKeycloak().tokenParsed['preferred_username'];
+					const axiosVault = axios.create({
+						baseURL: conf.VAULT.VAULT_BASE_URI,
+					});
+					const authorizeConfig = (token) => (config) => ({
+						...config,
+						headers: { 'X-Vault-Token': token },
+						'Content-Type': 'application/json;charset=utf-8',
+						Accept: 'application/json;charset=utf-8',
+					});
+					axiosVault.interceptors.request.use((config) =>
+						getVaultToken().then((token) =>
+							Promise.resolve(authorizeConfig(token)(config))
+						)
+					);
+					axiosVault(
+						`${VAULT_BASE_URI}/v1/onyxia-kv/${idep}/.onyxia/profile`
+					).catch(() => {
+						var password = generator.generate({
+							length: 20,
+							numbers: true,
+						});
+						axiosVault.post(`/v1/onyxia-kv/${idep}/.onyxia/profile`, {
+							password,
+						});
+					});
 				}
 				resolve(authenticated);
 			})
--- src/js/vault-client/vault-api.js
+++ src/js/vault-client/vault-api.js
@@ -1,36 +1,36 @@
-import axios from "axios";
-import { axiosAuth, getToken } from "js/utils";
-import { store } from "js/redux";
-import { newVaultToken } from "js/redux/actions";
-import conf from "../configuration";
+import axios from 'axios';
+import { axiosAuth, getToken } from 'js/utils';
+import { store } from 'js/redux';
+import { newVaultToken } from 'js/redux/actions';
+import conf from '../configuration';
 
 const VAULT_BASE_URI = conf.VAULT.VAULT_BASE_URI;
 
 class VaultAPI {
-  async getSecretsList(path = "") {
-    const { data } = await axiosVault({
-      method: "list",
-      url: `/v1/onyxia-kv${path}`
-    });
-    return data.data ? data.data.keys : [];
-  }
+	async getSecretsList(path = '') {
+		const { data } = await axiosVault({
+			method: 'list',
+			url: `/v1/onyxia-kv${path}`,
+		});
+		return data.data ? data.data.keys : [];
+	}
 
-  async getSecret(path = "") {
-    const { data } = await axiosVault({
-      method: "get",
-      url: `/v1/onyxia-kv${path}`
-    });
-    return data.data ? data.data : [];
-  }
+	async getSecret(path = '') {
+		const { data } = await axiosVault({
+			method: 'get',
+			url: `/v1/onyxia-kv${path}`,
+		});
+		return data.data ? data.data : [];
+	}
 
-  async createPath(path, payload) {
-    return axiosVault.put(`/v1/onyxia-kv${path}`, payload || { foo: "bar" });
-  }
+	async createPath(path, payload) {
+		return axiosVault.put(`/v1/onyxia-kv${path}`, payload || { foo: 'bar' });
+	}
 
-  async uploadSecret(path, payload) {
-    const { data } = await axiosVault.put(`/v1/onyxia-kv${path}`, payload);
-    return data.data ? data.data : [];
-  }
+	async uploadSecret(path, payload) {
+		const { data } = await axiosVault.put(`/v1/onyxia-kv${path}`, payload);
+		return data.data ? data.data : [];
+	}
 }
 
 export default VaultAPI;
@@ -41,36 +41,36 @@ const getLocalToken = () => store.getState().user.VAULT.VAULT_TOKEN;
  *
  */
 export const getVaultToken = async () =>
-  getLocalToken() ? Promise.resolve(getLocalToken()) : fetchVaultToken();
+	getLocalToken() ? Promise.resolve(getLocalToken()) : fetchVaultToken();
 
 const fetchVaultToken = async () => {
-  const {
-    auth: { client_token: token }
-  } = await axiosAuth.post(`${VAULT_BASE_URI}/v1/auth/jwt/login`, {
-    role: "onyxia-user",
-    jwt: await getToken()
-  });
-  store.dispatch(newVaultToken(token));
-  return token;
+	const {
+		auth: { client_token: token },
+	} = await axiosAuth.post(`${VAULT_BASE_URI}/v1/auth/jwt/login`, {
+		role: 'onyxia-user',
+		jwt: await getToken(),
+	});
+	store.dispatch(newVaultToken(token));
+	return token;
 };
 
 /**
  *
  */
 const axiosVault = axios.create({
-  baseURL: conf.VAULT.VAULT_BASE_URI
+	baseURL: conf.VAULT.VAULT_BASE_URI,
 });
 axiosVault.interceptors.request.use(
-  config =>
-    getVaultToken()
-      .then(token => Promise.resolve(authorizeConfig(token)(config)))
-      .catch(error => console.log(`Error ${error}`)),
-  error => Promise.reject(error)
+	(config) =>
+		getVaultToken()
+			.then((token) => Promise.resolve(authorizeConfig(token)(config)))
+			.catch((error) => console.log(`Error ${error}`)),
+	(error) => Promise.reject(error)
 );
 
-const authorizeConfig = token => config => ({
-  ...config,
-  headers: { "X-Vault-Token": token },
-  "Content-Type": "application/json;charset=utf-8",
-  Accept: "application/json;charset=utf-8"
+const authorizeConfig = (token) => (config) => ({
+	...config,
+	headers: { 'X-Vault-Token': token },
+	'Content-Type': 'application/json;charset=utf-8',
+	Accept: 'application/json;charset=utf-8',
 });
