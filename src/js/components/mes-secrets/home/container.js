8abcb96d5e55bedb717c30498dc86efe8fb33d17
NicoLaval <nicolas.laval@insee.fr>
2020-04-03 10:28:38 +0200

    Improve mes-secrets home page

--- src/js/components/mes-secrets/home/container.js
+++ src/js/components/mes-secrets/home/container.js
@@ -1,6 +1,9 @@
 import { connect } from 'react-redux';
 import MesSecrets from './component';
 
-const mapStateToProps = ({ user }) => ({ idep: user.IDEP });
+const mapStateToProps = ({ user }) => ({
+	idep: user.IDEP,
+	data: user.VAULT ? user.VAULT.DATA : undefined,
+});
 
 export default connect(mapStateToProps)(MesSecrets);
