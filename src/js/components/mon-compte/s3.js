9e4d5bd09694eeb9b196076a9db7d7438d8157d9
NicoLaval <nicolas.laval@insee.fr>
2020-04-05 21:01:30 +0200

    Fix #63

---
+++ src/js/components/mon-compte/s3.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import {
+	InputAdornment,
+	Input,
+	InputLabel,
+	IconButton,
+	FormControl,
+} from '@material-ui/core';
+import FileCopy from '@material-ui/icons/FileCopy';
+import AutorenewIcon from '@material-ui/icons/Autorenew';
+import * as clipboard from 'clipboard-polyfill';
+import D from 'js/i18n';
+
+const S3Field = ({ value, handleReset }) => {
+	if (!value) return null;
+	return (
+		<FormControl className="copy-field" style={{ width: '100%' }}>
+			<InputLabel>{D.pwdTitle}</InputLabel>
+			<Input
+				disabled
+				label={D.pwdTitle}
+				fullWidth
+				value={value}
+				endAdornment={
+					<InputAdornment position="end">
+						<IconButton
+							aria-label="rafraîchir le mot de passe"
+							onClick={handleReset}
+						>
+							<AutorenewIcon />
+						</IconButton>
+						<IconButton
+							aria-label="copier dans le presse papier"
+							onClick={() => clipboard.writeText(value)}
+						>
+							<FileCopy />
+						</IconButton>
+					</InputAdornment>
+				}
+			/>
+		</FormControl>
+	);
+};
+
+export default S3Field;
