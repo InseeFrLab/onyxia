b5e5b5e31308fc9e9dea9f1d2c172ac964029572
Olivier Levitt <ungawa14@gmail.com>
2020-04-09 18:01:15 +0200

    Fix username / idep confusion

--- src/js/components/mon-compte/export-credentials-kub.js
+++ src/js/components/mon-compte/export-credentials-kub.js
@@ -6,8 +6,8 @@ const exportTypes = [
 		text: (c) =>
 			`#!/bin/sh
 kubectl config set-cluster ${c.KUBERNETES.KUB_SERVER_NAME} --server=${c.KUBERNETES.KUB_SERVER_URL}
-kubectl config set-credentials ${c.USERNAME} --token ${c.KEYCLOAK.KC_ACCESS_TOKEN} 
-kubectl config set-context ${c.KUBERNETES.KUB_SERVER_NAME} --user=${c.USERNAME} --cluster=${c.KUBERNETES.KUB_SERVER_NAME} --namespace=${c.USERNAME}
+kubectl config set-credentials ${c.IDEP} --token ${c.KEYCLOAK.KC_ACCESS_TOKEN} 
+kubectl config set-context ${c.KUBERNETES.KUB_SERVER_NAME} --user=${c.IDEP} --cluster=${c.KUBERNETES.KUB_SERVER_NAME} --namespace=${c.IDEP}
 kubectl config use-context ${c.KUBERNETES.KUB_SERVER_NAME}`,
 	},
 ];
