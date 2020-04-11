c12ac629650d5e87195c924357218bcd15ac95cf
Melinee Treppoz-Salomon <59824653+MelineeTS@users.noreply.github.com>
2020-04-11 12:57:54 +0200

    Feature : delete all services (#89)

---
+++ src/js/i18n/dictionary/messervices.js
@@ -0,0 +1,10 @@
+const dictionary = {
+	deleteAllServices: {
+		fr: `Vous êtes sur le point de supprimer tous vos services. 
+            Les données associées seront perdues.`,
+		en: `You are about to delete all your services.
+            You will lose all data associated to them.`,
+	},
+};
+
+export default dictionary;
