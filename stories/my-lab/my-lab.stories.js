c2f6fd769ebf5453aa732c4c0610a2d4b24771d0
NicoLaval <nicolas.laval@insee.fr>
2020-04-13 09:37:54 +0200

    Update stories

--- stories/my-lab/my-lab.stories.js
+++ stories/my-lab/my-lab.stories.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import { withKnobs, object } from '@storybook/addon-knobs';
+import { withKnobs, object, boolean } from '@storybook/addon-knobs';
 import CarteMonService from 'js/components/commons/service-liste/carte-mon-service';
-import fakeService from './data/service.json';
+import fakeService from './service.json';
 import { MemoryRouter } from 'react-router-dom';
 import { muiTheme } from 'storybook-addon-material-ui';
 import createTheme from 'js/components/material-ui-theme';
-import 'js/components/app.scss';
-import 'js/components/commons/service-liste/liste-cartes.scss';
 
 export default {
 	title: 'Mylab',
@@ -19,5 +17,8 @@ export default {
 };
 
 export const WithMinimalData = () => (
-	<CarteMonService service={object('service', fakeService)} />
+	<CarteMonService
+		service={object('service', fakeService)}
+		wait={boolean('Wait', false)}
+	/>
 );
