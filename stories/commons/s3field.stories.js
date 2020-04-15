6e54a60736bdcf054e7235118b47f885b493ec3a
CChemin <cecile.chemin@insee.fr>
2020-04-15 10:04:18 +0200

    modif on s3field stories

--- stories/commons/s3field.stories.js
+++ stories/commons/s3field.stories.js
@@ -10,6 +10,18 @@ export default {
 
 export const BasicS3Field = () => <S3Field value={text('value', 'toto')} />;
 
+export const RenewableS3Field = () => <S3Field value={text('value', 'toto')} />;
+
+export const ListS3Field = () => <S3Field value={text('value', 'toto')} />;
+
 BasicS3Field.story = {
 	title: 'BasicS3Field',
+};
+
+RenewableS3Field.story = {
+	title: 'RenewableS3Field',
+};
+
+ListS3Field.story = {
+	title: 'ListS3Field',
 };
