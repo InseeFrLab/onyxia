87579c582ccbbbec8379a3906dc6d927a34348db
Nicolas Laval <nico.laval28@gmail.com>
2020-04-15 16:47:01 +0200

    Update preview.js

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,9 @@
+import React from 'react';
+import { MemoryRouter } from 'react-router';
+import { addDecorator } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs';
 import { muiTheme } from 'storybook-addon-material-ui';
 import createTheme from 'js/components/material-ui-theme';
-import React from 'react';
-import { addDecorator } from '@storybook/react';
-import { MemoryRouter } from 'react-router';
 
 addDecorator(withKnobs);
 addDecorator(muiTheme(createTheme()));
