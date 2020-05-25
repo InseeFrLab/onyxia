403161f0619859783f2d7b1cba5210a2f8258a4e
Olivier Levitt <ungawa14@gmail.com>
2020-05-25 13:47:56 +0200

    Get api configuration (#135)

---
+++ src/js/api/configuration.ts
@@ -0,0 +1,19 @@
+import apiPaths from 'js/configuration/api-paths';
+import { Region } from 'js/model/Region';
+import { axiosPublic } from 'js/utils';
+
+export interface Configuration {
+	regions: Region[];
+	build: Build;
+}
+
+export interface Build {
+	version: string;
+	timestamp: number;
+}
+
+export const getConfiguration = async () => {
+	return await axiosPublic
+		.get<Configuration>(apiPaths.configuration)
+		.then((resp) => (resp as unknown) as Configuration);
+};
