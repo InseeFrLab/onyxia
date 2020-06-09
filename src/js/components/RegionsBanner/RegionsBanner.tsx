4ddaead564d0afac212cd0a9c14f8dd296ba2504
Donatien Eneman <donatien.eneman@insee.fr>
2020-06-09 13:46:40 +0200

    bouton a droite

--- src/js/components/RegionsBanner/RegionsBanner.tsx
+++ src/js/components/RegionsBanner/RegionsBanner.tsx
@@ -4,17 +4,29 @@ import Alert from '@material-ui/lab/Alert';
 import {
 	IconButton,
 	Collapse,
-	Button,
 	MenuItem,
 	Select,
+	Grid,
 } from '@material-ui/core';
-import CloseIcon from '@material-ui/icons/Close';
 import { regionChanged } from 'js/redux/actions';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import ExpandLessIcon from '@material-ui/icons/ExpandLess';
+import { Region } from 'js/model/Region';
 
-const Selectedregions = (props) => {
+interface RegionsState {
+	regions: Region[];
+	selectedRegion?: Region;
+}
+
+interface props {
+	regions: RegionsState;
+}
+
+const Selectedregions = (props: props) => {
 	const regions = props.regions;
 	const dispatch = useDispatch();
-	const changeRegion = (id) => {
+
+	const changeRegion = (id: String) => {
 		dispatch(
 			regionChanged(regions.regions.filter((region) => region.id === id)[0])
 		);
@@ -25,7 +37,7 @@ const Selectedregions = (props) => {
 			labelId="demo-simple-select-label"
 			id="demo-simple-select"
 			value={regions.selectedRegion.id}
-			onChange={(e) => changeRegion(e.target.value)}
+			onChange={(e) => changeRegion(e.target.value as String)}
 		>
 			{regions.regions.map((region) => (
 				<MenuItem key={region.id} value={region.id}>
@@ -44,26 +56,36 @@ const RegionBanner = () => {
 	return regions && regions.selectedRegion ? (
 		<>
 			<Collapse in={open}>
-				<Alert
-					variant="filled"
-					severity="warning"
-					action={
-						<IconButton
-							aria-label="close"
-							color="inherit"
-							size="small"
-							onClick={() => setOpen(false)}
-						>
-							<CloseIcon fontSize="inherit" />
-						</IconButton>
-					}
-				>
+				<Alert variant="filled" severity="warning">
 					Vous déployez dans la région: <Selectedregions regions={regions} />
 				</Alert>
 			</Collapse>
-			<Button disabled={open} variant="outlined" onClick={() => setOpen(true)}>
-				Changer régions
-			</Button>
+			<Grid
+				container
+				direction="row"
+				justify="flex-end"
+				alignItems="flex-start"
+			>
+				{!open ? (
+					<IconButton
+						aria-label="show"
+						color="inherit"
+						size="small"
+						onClick={() => setOpen(true)}
+					>
+						<ExpandMoreIcon fontSize="inherit" />
+					</IconButton>
+				) : (
+					<IconButton
+						aria-label="show"
+						color="inherit"
+						size="small"
+						onClick={() => setOpen(false)}
+					>
+						<ExpandLessIcon fontSize="inherit" />
+					</IconButton>
+				)}
+			</Grid>
 		</>
 	) : (
 		<></>
