2d0296a917f39cbe057629fd0341c80b767d41d8
Donatien Eneman <donatien.eneman@insee.fr>
2020-06-09 15:21:56 +0200

    test

--- src/js/components/RegionsBanner/RegionsBanner.tsx
+++ src/js/components/RegionsBanner/RegionsBanner.tsx
@@ -1,63 +1,77 @@
-import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import Alert from '@material-ui/lab/Alert';
-import {
-	IconButton,
-	Collapse,
-	MenuItem,
-	Select,
-	Grid,
-} from '@material-ui/core';
-import { regionChanged } from 'js/redux/actions';
+import { IconButton, Collapse, Grid, Paper } from '@material-ui/core';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import ExpandLessIcon from '@material-ui/icons/ExpandLess';
 import { Region } from 'js/model/Region';
+import { getConfiguration } from 'js/api/configuration';
+import { RootState } from 'js/redux/';
 
 interface RegionsState {
 	regions: Region[];
 	selectedRegion?: Region;
 }
 
-interface props {
+interface Props {
 	regions: RegionsState;
 }
 
-const Selectedregions = (props: props) => {
-	const regions = props.regions;
-	const dispatch = useDispatch();
+// const Selectedregions = (props: Props) => {
+// 	const regions = props.regions;
+// 	const dispatch = useDispatch();
 
-	const changeRegion = (id: String) => {
-		dispatch(
-			regionChanged(regions.regions.filter((region) => region.id === id)[0])
-		);
-	};
+// 	const changeRegion = (id: String) => {
+// 		dispatch(
+// 			regionChanged(regions.regions.filter((region) => region.id === id)[0])
+// 		);
+// 	};
 
-	return regions && regions.selectedRegion ? (
-		<Select
-			labelId="demo-simple-select-label"
-			id="demo-simple-select"
-			value={regions.selectedRegion.id}
-			onChange={(e) => changeRegion(e.target.value as String)}
-		>
-			{regions.regions.map((region) => (
-				<MenuItem key={region.id} value={region.id}>
-					{region.name}
-				</MenuItem>
-			))}
-		</Select>
-	) : (
-		<></>
-	);
-};
+// 	return regions && regions.selectedRegion ? (
+// 		<Select
+// 			labelId="demo-simple-select-label"
+// 			id="demo-simple-select"
+// 			value={regions.selectedRegion.id}
+// 			onChange={(e) => changeRegion(e.target.value as String)}
+// 		>
+// 			{regions.regions.map((region) => (
+// 				<MenuItem key={region.id} value={region.id}>
+// 					{region.name}
+// 				</MenuItem>
+// 			))}
+// 		</Select>
+// 	) : (
+// 		<></>
+// 	);
+// };
 
 const RegionBanner = () => {
-	const regions = useSelector((store) => store.regions);
+	const regions = useSelector((store: RootState) => store.regions);
 	const [open, setOpen] = useState(true);
-	return regions && regions.selectedRegion ? (
+
+	useEffect(() => {
+		if (!regions.selectedRegion) {
+			getConfiguration().then();
+		}
+	}, [regions.selectedRegion]);
+
+	return regions?.selectedRegion ? (
 		<>
 			<Collapse in={open}>
 				<Alert variant="filled" severity="warning">
-					Vous déployez dans la région: <Selectedregions regions={regions} />
+					<Grid
+						container
+						direction="row"
+						justify="space-between"
+						alignItems="flex-start"
+					>
+						<Grid item xs={6}>
+							Vous déployez dans la région {regions.selectedRegion.name}
+						</Grid>
+						<Grid item xs={6}>
+							Vous déployez dans la région {regions.selectedRegion.name}
+						</Grid>
+					</Grid>
 				</Alert>
 			</Collapse>
 			<Grid
