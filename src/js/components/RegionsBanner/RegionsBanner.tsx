27994ec0cd5a24e6b786ab95c692b93d1deeb87d
Donatien Eneman <donatien.eneman@insee.fr>
2020-06-09 15:53:36 +0200

    end

--- src/js/components/RegionsBanner/RegionsBanner.tsx
+++ src/js/components/RegionsBanner/RegionsBanner.tsx
@@ -1,21 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import Alert from '@material-ui/lab/Alert';
-import { IconButton, Collapse, Grid, Paper } from '@material-ui/core';
+import {
+	IconButton,
+	Collapse,
+	Grid,
+	Paper,
+	Button,
+	Typography,
+} from '@material-ui/core';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import ExpandLessIcon from '@material-ui/icons/ExpandLess';
 import { Region } from 'js/model/Region';
 import { getConfiguration } from 'js/api/configuration';
 import { RootState } from 'js/redux/';
+import { useHistory } from 'react-router-dom';
 
-interface RegionsState {
-	regions: Region[];
-	selectedRegion?: Region;
-}
-
-interface Props {
-	regions: RegionsState;
-}
+// interface Props {
+// 	regions: RegionsState;
+// }
 
 // const Selectedregions = (props: Props) => {
 // 	const regions = props.regions;
@@ -48,30 +51,33 @@ interface Props {
 const RegionBanner = () => {
 	const regions = useSelector((store: RootState) => store.regions);
 	const [open, setOpen] = useState(true);
-
+	const history = useHistory();
 	useEffect(() => {
 		if (!regions.selectedRegion) {
-			getConfiguration().then();
+			getConfiguration();
 		}
 	}, [regions.selectedRegion]);
 
 	return regions?.selectedRegion ? (
 		<>
 			<Collapse in={open}>
-				<Alert variant="filled" severity="warning">
-					<Grid
-						container
-						direction="row"
-						justify="space-between"
-						alignItems="flex-start"
-					>
-						<Grid item xs={6}>
-							Vous déployez dans la région {regions.selectedRegion.name}
-						</Grid>
-						<Grid item xs={6}>
-							Vous déployez dans la région {regions.selectedRegion.name}
-						</Grid>
-					</Grid>
+				<Alert
+					variant="filled"
+					severity="warning"
+					action={
+						<Button
+							aria-label="a-propos"
+							color="inherit"
+							size="small"
+							onClick={() => history.push('/about')}
+						>
+							Changer
+						</Button>
+					}
+				>
+					<Typography>
+						Vous déployez dans la région {regions.selectedRegion.name}
+					</Typography>
 				</Alert>
 			</Collapse>
 			<Grid
