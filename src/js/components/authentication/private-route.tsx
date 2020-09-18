768ecdd4be95943450277472cd094453bc885b1a
Joseph Garrone <joseph.garrone.gj@gmail.com>
2020-09-18 17:57:55 +0200

    private-route componenent, not refactored, just make it build ok

--- src/js/components/authentication/private-route.tsx
+++ src/js/components/authentication/private-route.tsx
@@ -1,28 +1,33 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { displayLogin, setRedirectUri, setFavicon } from 'js/redux/actions';
-import { getToken } from 'js/utils/localStorageToken';
+//import { displayLogin, setRedirectUri, setFavicon } from 'js/redux/actions';
+import { actions as appActions } from "js/redux/app";
+//import { actions as userActions } from "js/redux/user";
+
+
+//import { getToken } from 'js/utils/localStorageToken';
+import * as localStorageToken from "js/utils/localStorageToken";
 import { ONYXIA_FAVICON } from 'js/components/commons/favicon';
 import { getKeycloak } from 'js/utils';
 
-const createPrivateRoute = (RouterContext) => (props) => (
+const createPrivateRoute = (RouterContext: any) => (props: any) => (
 	<PrivateRoute {...props} routerContext={RouterContext} />
 );
 
 class PrivateRoute extends React.Component {
 	state = { isToken: false };
-	constructor(props) {
+	constructor(props: any) {
 		super(props);
 		if (props.faviconUrl !== ONYXIA_FAVICON.onyxia) {
-			this.props.setFavicon(ONYXIA_FAVICON.onyxia);
+			(this.props as any).setFavicon(ONYXIA_FAVICON.onyxia);
 		}
 	}
 	static getDerivedStateFromProps(
-		{ authenticated, location, setRedirectUri, displayLogin },
-		state
+		{ authenticated, location, setRedirectUri, displayLogin }: any,
+		state: any
 	) {
-		const token = getToken();
+		const token = localStorageToken.get();
 		const isToken = token && token !== 'undefined';
 
 		if (!authenticated && !isToken) {
@@ -43,10 +48,10 @@ class PrivateRoute extends React.Component {
 			routerContext: RouterContext,
 			// path,
 			...rest
-		} = this.props;
+		} = this.props as any;
 		return (
 			<RouterContext.Consumer>
-				{({ pathname }) =>
+				{({ pathname }: any) =>
 					!authenticated && !this.state.isToken ? (
 						<Redirect to={pathname} />
 					) : (
@@ -58,17 +63,17 @@ class PrivateRoute extends React.Component {
 	}
 }
 
-const mapStateToProps = (state, ownProps) => {
+const mapStateToProps = (state: any, ownProps: any) => {
 	const { authenticated, faviconUrl } = state.app;
 
 	return { ...ownProps, authenticated, faviconUrl };
 };
 
 const dispatchToProps = {
-	displayLogin,
-	setRedirectUri,
-	setFavicon,
+	"displayLogin": appActions.displayLogin,
+	"setRedirectUri": appActions.setRedirectUri,
+	"setFavicon": appActions.setFavicon,
 };
 
-export default (RouterContext) =>
+export default (RouterContext: any) =>
 	connect(mapStateToProps, dispatchToProps)(createPrivateRoute(RouterContext));
