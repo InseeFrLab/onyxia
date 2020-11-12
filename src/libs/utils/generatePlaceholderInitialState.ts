c3c03d18c74a569454bab9c78a990518ce052dc3
Joseph Garrone <joseph.garrone.gj@gmail.com>
2020-11-12 20:30:26 +0100

    Thow an error if attempting to set values on un-initialized state

--- src/libs/utils/generatePlaceholderInitialState.ts
+++ src/libs/utils/generatePlaceholderInitialState.ts
@@ -1,13 +1,18 @@
 
-  export function generatePlaceholderInitialState<State extends object>(
-      debugMessage?: string
-  ): State {
-      return new Proxy<State>(
-          {} as any, 
-          {
-              "get": (...[,prop])=> {
-                throw new Error(`Cannot access ${String(prop)} yet ${debugMessage??""}`);
-              }
-          }
-      );
-  }
+export function generatePlaceholderInitialState<State extends object>(
+    debugMessage?: string
+): State {
+
+    const throwError = (prop: string | number | symbol): never => {
+        throw new Error(`Cannot access ${String(prop)} yet ${debugMessage ?? ""}`);
+    }
+
+    return new Proxy<State>(
+        {} as any,
+        {
+            "get": (...[, prop]) => throwError(prop),
+            "set": (...[, prop]) => throwError(prop)
+        }
+    );
+
+}
