4c32aefa2d50dbf99a15c6ea4ad52afa9ccccb62
Joseph Garrone <joseph.garrone.gj@gmail.com>
2020-12-01 11:22:06 +0100

    Remove icons due to case issue

--- src/app/atoms/Icons/TourIcon.tsx
+++
@@ -1,9 +0,0 @@
-
-import React from "react";
-import SvgIcon from "@material-ui/core/SvgIcon";
-import type { SvgIconProps } from "@material-ui/core/SvgIcon";
-import { ReactComponent as TourIconSvg } from "./svg/VisiteGuidee.svg";
-
-export function TourIcon(props: SvgIconProps) {
-    return <SvgIcon  {...props} component={TourIconSvg}/>;
-}
