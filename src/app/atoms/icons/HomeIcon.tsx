cfaa93e3a193ae485398f408ee5b9cb82474b1de
Joseph Garrone <joseph.garrone.gj@gmail.com>
2020-12-01 11:24:42 +0100

    Re add icons

---
+++ src/app/atoms/icons/HomeIcon.tsx
@@ -0,0 +1,9 @@
+
+import React from "react";
+import SvgIcon from "@material-ui/core/SvgIcon";
+import type { SvgIconProps } from "@material-ui/core/SvgIcon";
+import { ReactComponent as IconSvg } from "./svg/HomeIconStock.svg";
+
+export function HomeIcon(props: SvgIconProps) {
+    return <SvgIcon  {...props} component={IconSvg}/>;
+}
