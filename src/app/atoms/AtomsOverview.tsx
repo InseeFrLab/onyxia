32e5b682a51fe1e8082edbaf4329c96edba816a5
Joseph Garrone <joseph.garrone.gj@gmail.com>
2020-12-02 08:58:15 +0100

    Factorize theming setup

---
+++ src/app/atoms/AtomsOverview.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { makeStyles, createStyles } from "@material-ui/core/styles";
+import { AppIcon } from "./AppIcon";
+import type { Props as AppIconProps } from "./AppIcon";
+import { id } from "evt/tools/typeSafety/id";
+import { useIsDarkModeEnabled } from "app/redux/hooks";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Switch from "@material-ui/core/Switch";
+import { useTheme } from "@material-ui/core/styles";
+
+const appIconTypes = id<AppIconProps["type"][]>(
+    [
+        "tour", "services", "secrets", "profile",
+        "lab", "info", "home", "trainings", "files",
+        "collaborationTools", "bash"
+    ]
+);
+
+const useStyles = makeStyles(theme =>
+    createStyles({
+        "root": {
+            "& > svg": {
+                "margin": theme.spacing(2)
+            }
+        }
+    })
+);
+
+export function AtomsOverview() {
+
+    const { isDarkModeEnabled, setIsDarkModeEnabled } = useIsDarkModeEnabled();
+    const classes = useStyles();
+    const theme = useTheme();
+
+    return (
+        <>
+
+            <FormControlLabel
+                style={{ "margin": theme.spacing(2) }}
+                control={
+                    <Switch
+                        checked={isDarkModeEnabled}
+                        onChange={event => setIsDarkModeEnabled(event.target.checked)}
+                        color="primary"
+                    />
+                }
+                label={`Dark mode is currently ${isDarkModeEnabled ? "enabled" : "disabled"}`}
+            />
+
+            <h1>{"<AppIcon />"}</h1>
+            <>
+                {appIconTypes.map(appIconType => (
+                    <div className={classes.root} key={appIconType}>
+                        {(["inherit", "disabled", "primary", "secondary", "action", "error"] as const).map(color =>
+                            <AppIcon type={appIconType} color={color} key={color} />
+                        )}
+                    </div>
+                ))}
+            </>
+        </>
+    );
+}
--- src/app/atoms/icons/FilesIcon.tsx
+++
@@ -1,9 +0,0 @@
-
-import React from "react";
-import SvgIcon from "@material-ui/core/SvgIcon";
-import type { SvgIconProps } from "@material-ui/core/SvgIcon";
-import { ReactComponent as IconSvg } from "./svg/Files.svg";
-
-export function FilesIcon(props: SvgIconProps) {
-    return <SvgIcon  {...props} component={IconSvg}/>;
-}
