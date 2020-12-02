6f8c1ea3216b60fead3678bc459cb9da08281ba7
Joseph Garrone <joseph.garrone.gj@gmail.com>
2020-12-02 21:55:59 +0100

    storybook setup

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,15 @@
+
+
 module.exports = {
-	stories: ['../src/**/*.stories.(js|jsx|ts|tsx|mdx)'],
-	addons: [
-		'@storybook/preset-create-react-app',
-		'@storybook/addon-actions',
-		'@storybook/addon-links',
-		'@storybook/addon-knobs',
-		{
-			name: '@storybook/addon-docs',
-			options: {
-				configureJSX: true,
-			},
-		},
-	],
+  "stories": [
+    "../src/stories/**/*.stories.mdx",
+    "../src/stories/**/*.stories.@(ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app"
+  ],
+  // https://github.com/storybookjs/storybook/issues/4672#issuecomment-512817364
+  "loader": require.resolve("babel-loader", { "paths": ["node_modules/react-scripts"] }),
 };
