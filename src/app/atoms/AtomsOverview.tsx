2e75a888249391597eeeda3ec9912d9137be2590
marchufschmitt <marc.hufschmitt@hotmail.fr>
2020-12-02 12:57:16 +0100

    service badges setup

--- src/app/atoms/AtomsOverview.tsx
+++ src/app/atoms/AtomsOverview.tsx
@@ -1,35 +1,14 @@
 import React from "react";
-import { makeStyles, createStyles } from "@material-ui/core/styles";
-import { AppIcon } from "./AppIcon";
-import type { Props as AppIconProps } from "./AppIcon";
-import { id } from "evt/tools/typeSafety/id";
 import { useIsDarkModeEnabled } from "app/redux/hooks";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Switch from "@material-ui/core/Switch";
 import { useTheme } from "@material-ui/core/styles";
-
-const appIconTypes = id<AppIconProps["type"][]>(
-    [
-        "tour", "services", "secrets", "profile",
-        "lab", "info", "home", "trainings", "files",
-        "collaborationTools", "bash"
-    ]
-);
-
-const useStyles = makeStyles(theme =>
-    createStyles({
-        "root": {
-            "& > svg": {
-                "margin": theme.spacing(2)
-            }
-        }
-    })
-);
+import { AppIconOverview } from "./AppIconOverview";
+import { ServiceBadgeOverview } from "./ServiceBadgesOverview";
 
 export function AtomsOverview() {
 
     const { isDarkModeEnabled, setIsDarkModeEnabled } = useIsDarkModeEnabled();
-    const classes = useStyles();
     const theme = useTheme();
 
     return (
@@ -47,16 +26,8 @@ export function AtomsOverview() {
             />
 
             <>
-                {appIconTypes.map(appIconType => (
-                    <>
-                        <h3><code>{`<AppIcon type="${appIconType}"/>`}</code></h3>
-                        <div className={classes.root} key={appIconType}>
-                            {(["inherit", "disabled", "primary", "secondary", "action", "error"] as const).map(color =>
-                                <AppIcon type={appIconType} color={color} key={color} />
-                            )}
-                        </div>
-                    </>
-                ))}
+                <ServiceBadgeOverview />
+                <AppIconOverview />
             </>
         </>
     );
