2404838aaa381c5da00fa7e9255bf2477f9e55ca
Joseph Garrone <joseph.garrone.gj@gmail.com>
2020-12-04 13:59:23 +0100

    rename res directory -> assets

--- src/app/atoms/AppIcon.tsx
+++ src/app/atoms/AppIcon.tsx
@@ -2,17 +2,17 @@
 
 import React from "react";
 import SvgIcon from "@material-ui/core/SvgIcon";
-import { ReactComponent as tourSvg } from "app/res/svg/AssistedTour.svg";
-import { ReactComponent as servicesSvg } from "app/res/svg/Services.svg";
-import { ReactComponent as secretsSvg } from "app/res/svg/Secrets.svg";
-import { ReactComponent as profileSvg } from "app/res/svg/Profile.svg";
-import { ReactComponent as labSvg } from "app/res/svg/Lab.svg";
-import { ReactComponent as infoSvg } from "app/res/svg/Info.svg";
-import { ReactComponent as homeSvg } from "app/res/svg/Home.svg";
-import { ReactComponent as trainingsSvg } from "app/res/svg/Trainings.svg";
-import { ReactComponent as filesSvg } from "app/res/svg/Files.svg";
-import { ReactComponent as collaborationToolsSvg } from "app/res/svg/CollaborationTools.svg";
-import { ReactComponent as bashSvg } from "app/res/svg/Bash.svg";
+import { ReactComponent as tourSvg } from "app/assets/svg/AssistedTour.svg";
+import { ReactComponent as servicesSvg } from "app/assets/svg/Services.svg";
+import { ReactComponent as secretsSvg } from "app/assets/svg/Secrets.svg";
+import { ReactComponent as profileSvg } from "app/assets/svg/Profile.svg";
+import { ReactComponent as labSvg } from "app/assets/svg/Lab.svg";
+import { ReactComponent as infoSvg } from "app/assets/svg/Info.svg";
+import { ReactComponent as homeSvg } from "app/assets/svg/Home.svg";
+import { ReactComponent as trainingsSvg } from "app/assets/svg/Trainings.svg";
+import { ReactComponent as filesSvg } from "app/assets/svg/Files.svg";
+import { ReactComponent as collaborationToolsSvg } from "app/assets/svg/CollaborationTools.svg";
+import { ReactComponent as bashSvg } from "app/assets/svg/Bash.svg";
 
 export type Props = {
     /** Design which icon should be displayed */
