2c2bf9d2ffbb0252825dc974294a8783cfdddc61
Joseph Garrone <joseph.garrone.gj@gmail.com>
2020-12-06 13:41:46 +0100

    Implement molecules/ExplorerItems

---
+++ src/app/molecules/ExplorerItems.tsx
@@ -0,0 +1,68 @@
+
+import React, { useMemo } from "react";
+import Grid from '@material-ui/core/Grid';
+import type { Props as ExporterItemProps } from "../atoms/ExplorerItem";
+import { explorerItemFactory } from "../atoms/explorerItemFactory";
+import { assert } from "evt/tools/typeSafety/assert";
+import { allUniq } from "evt/tools/reducers/allUniq";
+import memoize from "memoizee";
+
+export type Props = {
+    visualRepresentationOfAFile: ExporterItemProps["visualRepresentationOfAFile"];
+    /** Assert all uniq */
+    files: string[];
+    /** Assert all uniq */
+    directories: string[];
+    onOpen(params: { kind: "file" | "directory"; basename: string; }): void;
+};
+
+export function ExplorerItems(props: Props) {
+
+    const { visualRepresentationOfAFile, files, directories, onOpen } = props;
+
+    const { ExplorerItem } = useMemo(
+        () => explorerItemFactory({ visualRepresentationOfAFile }),
+        [visualRepresentationOfAFile]
+    );
+
+    useMemo(
+        () => assert(
+            files.reduce(...allUniq()) &&
+            directories.reduce(...allUniq()),
+            "Can't have two file or directory with the same name"
+        ),
+        [files, directories]
+    );
+
+    const onClickFactory = useMemo(
+        () => memoize(
+            (kind: "file" | "directory", basename: string) =>
+                () => onOpen({ kind, basename })
+        ),
+        [onOpen]
+    );
+
+    return (
+        <Grid container spacing={5}>
+            { directories.map(basename =>
+                <Grid item key={"d" + basename}>
+                    <ExplorerItem
+                        kind="directory"
+                        basename={basename}
+                        onClick={onClickFactory("directory", basename)}
+                    />
+                </Grid>
+            )}
+            {files.map(basename =>
+                <Grid item key={"f" + basename}>
+                    <ExplorerItem
+                        kind="file"
+                        basename={basename}
+                        onClick={onClickFactory("file", basename)}
+                    />
+                </Grid>
+            )}
+        </Grid>
+    );
+
+}
