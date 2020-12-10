a33698f32a4a80430a2b8523aa6db4c9216dfee9
Joseph Garrone <joseph.garrone.gj@gmail.com>
2020-12-10 16:05:13 +0100

    Add unstyled page header

---
+++ src/app/atoms/AppLink.tsx
@@ -0,0 +1,47 @@
+
+
+
+import React from "react";
+import { makeStyles, createStyles } from "@material-ui/core/styles";
+import Link from "@material-ui/core/Link";
+import { LinkClassKey } from "@material-ui/core/Link";
+import type { Id, Optional } from "evt/tools/typeSafety";
+import { noUndefined } from "app/utils/noUndefined";
+
+export type Props = {
+    /** Usually a plain text, that represents the text of the link */
+    children: React.ReactNode;
+    href: string;
+};
+
+export const defaultProps: Optional<Props> = {
+};
+
+
+const useStyles = makeStyles(
+    () => createStyles<Id<LinkClassKey, "root">, {}>({
+        "root": {
+        }
+    })
+);
+
+
+// NOTE for later integration with type route
+// https://material-ui.com/guides/composition/#link
+// https://github.com/typehero/type-route#step-4-navigate-between-routes
+
+export function AppLink(props: Props) {
+
+    const { children, href } = { ...defaultProps, ...noUndefined(props) };
+
+    const classes = useStyles();
+
+    return <Link classes={classes} href={href}>{children}</Link>
+
+}
+
+
+
+
+
+
---
+++ src/app/molecules/PageHeader.tsx
@@ -0,0 +1,27 @@
+
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+import { Props as AppIconProps }  from "../atoms/AppIcon";
+import { AppIcon } from "../atoms/AppIcon";
+
+export type Props = {
+    icon: AppIconProps["type"] | undefined;
+    text1: React.ReactNode;
+    text2: React.ReactNode;
+    text3: React.ReactNode;
+};
+
+export function PageHeader(props: Props) {
+
+    const { icon, text1, text2, text3 } = props;
+
+    return (
+        <>
+            <Typography variant="h3">{icon === undefined ? null : <AppIcon type={icon} />}{text1}</Typography>
+            <Typography variant="h6">{text2}</Typography>
+            <Typography variant="subtitle1">{text3}</Typography>
+        </>
+    );
+
+}
+
