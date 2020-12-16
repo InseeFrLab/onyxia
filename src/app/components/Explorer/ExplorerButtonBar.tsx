a1451a2767d5a25f4d174556712a27843ba04e23
Joseph Garrone <joseph.garrone.gj@gmail.com>
2020-12-16 03:29:04 +0100

    Actually staging changes

--- src/app/components/Explorer/ExplorerButtonBar.tsx
+++ src/app/components/Explorer/ExplorerButtonBar.tsx
@@ -3,8 +3,9 @@ import React, { useMemo } from "react";
 import { Button } from "app/components/designSystem/Button";
 import { useTranslation } from "app/i18n/useTranslations";
 import memoize from "memoizee";
+import { id } from "evt/tools/typeSafety/id";
 
-export type Action = "rename" | "create secret" | "create directory" | "delete" | "copy path" 
+export type Action = "rename" | "create secret" | "create directory" | "delete" | "copy path"
 
 export type Props = {
 
@@ -15,31 +16,47 @@ export type Props = {
 
 export function ExplorerButtonBar(props: Props) {
 
+    const { callback, isThereAnItemSelected } = props;
+
     const { t } = useTranslation("ExplorerButtonBar");
 
-    const onClickFactory= useMemo(
+    const onClickFactory = useMemo(
         () => memoize(
             (action: Action) =>
-                () => { }
+                () => callback({ action })
         ),
-        []
+        [callback]
     );
 
     return (
-        <>
-            <Button >{t("create secret")}</Button>
-        </>
+        <> { ([
+            "rename",
+            "create secret",
+            "create directory",
+            "delete",
+            "copy path"
+        ] as const).map(action =>
+            <Button
+                icon={(() => {
+                    switch (action) {
+                        case "copy path": return "info" as const;
+                        case "create directory": return "info";
+                        case "create secret": return "info";
+                        case "delete": return "info";
+                        case "rename": return "info";
+                    }
+                })()}
+                disabled={!isThereAnItemSelected && id<Action[]>(["copy path", "delete", "rename"]).includes(action)}
+                key={action}
+                onClick={onClickFactory(action)}
+            >
+                {t(action)}
+            </Button>
+        )} </>
     );
-
-
-
-
 
 }
 
 export declare namespace ExplorerButtonBar {
-
     export type I18nScheme = Record<Action, undefined>;
-
-
 }
