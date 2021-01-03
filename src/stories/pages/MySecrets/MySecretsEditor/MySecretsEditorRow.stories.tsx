83be3fcdf4233fb4fd496395739b6782cd08bd63
Joseph Garrone <joseph.garrone.gj@gmail.com>
2021-01-03 23:06:57 +0100

    Stories for MySecretsEditorRow

--- src/stories/pages/MySecrets/MySecretsEditor/MySecretsEditorRow.stories.tsx
+++ src/stories/pages/MySecrets/MySecretsEditor/MySecretsEditorRow.stories.tsx
@@ -1,7 +1,9 @@
 
 import { MySecretsEditorRow } from "app/pages/MySecrets/MySecretsEditor/MySecretsEditorRow";
-import { getStoryFactory } from "stories/geStory";
+import { getStoryFactory, logCallbacks } from "stories/geStory";
 import { sectionName } from "./sectionsName";
+import { getIsValidStrValue, getIsValidKey } from "app/pages/MySecrets/MySecretsEditor";
+import { Evt } from "evt";
 
 const { meta, getStory } = getStoryFactory({
     sectionName,
@@ -11,5 +13,43 @@ const { meta, getStory } = getStoryFactory({
 
 export default meta;
 
-export const Vue1 = getStory({});
+const baseParams: Parameters<typeof getStory>[0] = {
+    getIsValidStrValue,
+    "isLocked": false,
+    "keyOfSecret": "FOO_BAR",
+    "strValue": "hello world",
+    "getResolvedValue": ({ strValue }) => strValue.replace(/"/g, ""),
+    "getIsValidAndAvailableKey": getIsValidKey,
+    "evtAction": new Evt(),
+    ...logCallbacks([
+        "onEdit",
+        "onDelete"
+    ])
+};
 
+export const VueDefault = getStory(baseParams);
+
+export const { VueEditing } = (() => {
+
+    const evtAction: (typeof baseParams)["evtAction"] = new Evt();
+
+    const VueEditing = getStory({
+        ...baseParams,
+        evtAction
+    });
+
+    Evt.asPostable(evtAction).post("ENTER EDITING STATE");
+
+    return { VueEditing };
+
+})();
+
+export const VueLongValue = getStory({
+    ...baseParams,
+    "strValue": [...Array(30)].map(() => Math.random().toString(36)[2]).join('')
+});
+
+export const VueLongKey = getStory({
+    ...baseParams,
+    "keyOfSecret": "thisIsAVeryLongKeyToSeeHowItShouldBeDisplayed"
+});
