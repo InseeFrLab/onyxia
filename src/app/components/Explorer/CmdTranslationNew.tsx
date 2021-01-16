2d3c413251674ecb8e049b789ee887a6b9be67b5
Joseph Garrone <joseph.garrone.gj@gmail.com>
2021-01-16 09:44:33 +0100

    New cmdTranslation completed

--- src/app/components/Explorer/CmdTranslationNew.tsx
+++ src/app/components/Explorer/CmdTranslationNew.tsx
@@ -10,8 +10,6 @@ import MuiCircularProgress from "@material-ui/core/CircularProgress";
 import { IconButton } from "app/components/designSystem/IconButton";
 import { Icon } from "app/components/designSystem/Icon";
 
-
-
 export type Props = {
 	className: string;
 	evtTranslation: NonPostableEvt<{
@@ -23,71 +21,114 @@ export type Props = {
 	maxHeight: number;
 };
 
-
-
 const useStyles = makeStyles(
-	theme => createStyles<
-		"root" | "iconButton" | "limeGreen" |
-		"white" | "circularLoading" | "collapsedPanel" | "expandedPanel" | "header" | "dollar",
-		Props & { headerHeight: number; isExpended: boolean; }
-	>({
-		"root": {
-		},
-		"iconButton": ({ isExpended }) => ({
-			"& svg": {
-				"color": theme.custom.colors.palette.limeGreen.main,
-				"transition": theme.transitions.create(
-					["transform"],
-					{ "duration": theme.transitions.duration.short }
-				),
-				"transform": isExpended ?
-					"rotate(-180deg)" :
-					"rotate(0)",
-			},
-			"&:hover": {
-				"& svg": {
-					"color": theme.custom.colors.palette.whiteSnow.white,
-				}
-			}
-		}),
-		"limeGreen": {
-			"color": theme.custom.colors.palette.limeGreen.main
-		},
-		"white": {
-			"color": theme.custom.colors.palette.whiteSnow.white
-		},
-		"circularLoading": {
-			"color": theme.custom.colors.palette.whiteSnow.main
-		},
-		"collapsedPanel": {
-			"maxHeight": 0,
-			"overflow": "hidden",
-			"transform": "scaleY(0)",
-			"transition": [
-				"all 150ms cubic-bezier(0.4, 0, 0.2, 1)", 
-				"all 150ms cubic-bezier(0.4, 0, 0.2, 1)"
-			].join(", "),
-		},
-		"expandedPanel": ({ maxHeight, headerHeight }) => ({
-			"maxHeight": maxHeight - headerHeight,
-			"backgroundColor": theme.custom.colors.palette.midnightBlue.light,
-			"overflow": "scroll",
-			"transition": "transform 150ms cubic-bezier(0.4, 0, 0.2, 1)",
-			"& pre": {
-				"whiteSpace": "pre-wrap",
-				"wordBreak": "break-all"
-			},
-			"transform": "scaleY(1)",
-			"transformOrigin":"top"
-		}),
-		"header": {
-			"backgroundColor": theme.custom.colors.palette.midnightBlue.main
-		},
-		"dollar": {
-			"border": "1px solid white",
-		}
+	theme => {
 
-	})
+		const borderRadius = `0 0 0 30px`;
+
+		return createStyles<
+			"iconButton" | "circularLoading" | "collapsedPanel" | "expandedPanel" | "header" | "lastTranslatedCmd" | "dollarContainer" | "entryRoot" | "preWrapper" | "bottomRef",
+			Props & { headerHeight: number; isExpended: boolean; }>({
+				"iconButton": ({ isExpended }) => ({
+					"& svg": {
+						"color": theme.custom.colors.palette.limeGreen.main,
+						"transition": theme.transitions.create(
+							["transform"],
+							{ "duration": theme.transitions.duration.short }
+						),
+						"transform": isExpended ?
+							"rotate(-180deg)" :
+							"rotate(0)",
+					},
+					"&:hover": {
+						"& svg": {
+							"color": theme.custom.colors.palette.whiteSnow.white,
+						}
+					},
+					"& .MuiTouchRipple-root": {
+						"color": theme.custom.colors.palette.limeGreen.main,
+					},
+				}),
+				"circularLoading": {
+					"color": theme.custom.colors.palette.whiteSnow.main
+				},
+				"collapsedPanel": {
+					"maxHeight": 0,
+					"overflow": "hidden",
+					"transform": "scaleY(0)",
+					"transition": [
+						"all 150ms cubic-bezier(0.4, 0, 0.2, 1)",
+						"all 150ms cubic-bezier(0.4, 0, 0.2, 1)"
+					].join(", "),
+				},
+				"expandedPanel": ({ maxHeight, headerHeight }) => ({
+					"maxHeight": maxHeight - headerHeight,
+					"backgroundColor": theme.custom.colors.palette.midnightBlue.light,
+					"overflow": "scroll",
+					"transition": "transform 150ms cubic-bezier(0.4, 0, 0.2, 1)",
+					"& pre": {
+						"whiteSpace": "pre-wrap",
+						"wordBreak": "break-all"
+					},
+					"transform": "scaleY(1)",
+					"transformOrigin": "top",
+					borderRadius
+				}),
+				"header": ({ isExpended }) => ({
+					"backgroundColor": theme.custom.colors.palette.midnightBlue.main,
+					...(!isExpended ? {} : { borderRadius }),
+					"borderRadius": `0 0 0 ${isExpended ? 0 : 30}px`,
+					"display": "flex",
+					"alignItems": "center",
+					//"border": "1px solid white"
+
+				}),
+				"lastTranslatedCmd": {
+					"flex": 1,
+					"whiteSpace": "nowrap",
+					"overflow": "hidden",
+					"textOverflow": "ellipsis",
+					"fontFamily": "monospace",
+					//"border": "1px solid white",
+					"color": theme.custom.colors.palette.limeGreen.main
+				},
+				"dollarContainer": {
+
+					"width": 70,
+					//"border": "1px solid white",
+					"textAlign": "end",
+					"paddingRight": 10,
+
+					"& svg": {
+						//"border": "1px solid white",
+					}
+				},
+				"entryRoot": {
+					"display": "flex",
+					//"border": "1px solid white"
+				},
+				"preWrapper": {
+					"flex": 1,
+					"& pre:nth-of-type(1)": {
+						"color": theme.custom.colors.palette.limeGreen.main,
+						"marginTop": 2
+					},
+					"& pre:nth-of-type(2)": {
+						"color": theme.custom.colors.palette.whiteSnow.white
+					}
+
+				},
+				"bottomRef": {
+					"float": "left",
+					"clear": "both"
+				}
+
+
+
+
+			});
+
+	}
 );
 export function CmdTranslation(props: Props) {
 
@@ -174,32 +215,21 @@ export function CmdTranslation(props: Props) {
 		[isExpended, evtTranslation.postCount]
 	);
 
+	//TODO: see if classes are recomputed every time because ref object changes
 	const classes = useStyles({ ...props, headerHeight, isExpended });
 
 	return (
 		<div className={className}>
-			<div style={{ "display": "flex", "alignItems": "center", "border": "1px solid white" }} ref={headerRef} className={classes.header}>
+			<div ref={headerRef} className={classes.header}>
 
-				<div style={{ "width": 50, /*"textAlign": "center",*/ "border": "1px solid white", "textAlign": "end", "paddingRight": 10 }} className={classes.limeGreen}>
-					<Icon type="attachMoney" color="limeGreen" className={classes.dollar} fontSize="small" />
-
-
-					{/*<code className={classes.limeGreen}>$</code>*/}
-
+				<div className={classes.dollarContainer} >
+					<Icon type="attachMoney" color="limeGreen" fontSize="small" />
 				</div>
 
-				<div className={classes.limeGreen} style={{
-					"flex": 1,
-					"whiteSpace": "nowrap",
-					"overflow": "hidden",
-					"textOverflow": "ellipsis",
-					"fontFamily": "monospace",
-					"border": "1px solid white"
-				}}>
+				<div className={classes.lastTranslatedCmd}>
 					{lastTranslatedCmd}
 				</div>
 
-				{/* inline block */}
 				<IconButton
 					onClick={toggleIsExpended}
 					type="expandMore"
@@ -207,27 +237,23 @@ export function CmdTranslation(props: Props) {
 				/>
 
 			</div>
-
 			<div className={
 				isExpended ?
 					classes.expandedPanel :
 					classes.collapsedPanel
 			} >
-
 				{translationHistory.map(
 					({ cmdId, cmd, resp }) =>
-						<div key={cmdId} style={{ "display": "flex", "border": "1px solid white" }}>
+						<div key={cmdId} className={classes.entryRoot}>
 
-							<div style={{ "width": 50, "border": "1px solid white", "textAlign": "end", "paddingRight": 10 }}>
-
-								<Icon type="attachMoney" color="limeGreen" className={classes.dollar} fontSize="small" />
-
+							<div className={classes.dollarContainer}>
+								<Icon type="attachMoney" color="limeGreen" fontSize="small" />
 							</div>
-							<div style={{ "flex": 1 }}>
-								<pre className={classes.limeGreen} style={{ "marginTop": 4 }}>
+							<div className={classes.preWrapper}>
+								<pre>
 									{cmd}
 								</pre>
-								<pre className={classes.white}>
+								<pre>
 									{resp === undefined ?
 										<MuiCircularProgress
 											classes={{ "root": classes.circularLoading }}
@@ -239,7 +265,7 @@ export function CmdTranslation(props: Props) {
 
 						</div>
 				)}
-				<div style={{ "float": "left", "clear": "both" }} ref={bottomRef} />
+				<div className={classes.bottomRef} ref={bottomRef} />
 
 			</div>
 
