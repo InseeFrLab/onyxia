237297d7417ab57da2fa35aa95d83dffd967dcf0
Joseph Garrone <joseph.garrone.gj@gmail.com>
2021-01-27 14:26:21 +0100

    update generate entrypoint

--- scripts/generate-entrypoint.js
+++ scripts/generate-entrypoint.js
@@ -7,14 +7,14 @@ fs.readFile('.env', 'utf8', function (err, contents) {
 		.map((line) => line.split('='))
 		.filter((data) => data.length === 2)
 		.map(
-			([key, value]) =>
+			([key]) =>
 				`echo "window._env_['${key.replace(
 					'REACT_APP_',
 					''
-				)}'] = '\$${key.replace(
+				)}'] = \\"$${key.replace(
 					'REACT_APP_',
 					''
-				)}';" >> /usr/share/nginx/html/env-config.js`
+				)}\\";" >> /usr/share/nginx/html/env-config.js`
 		);
 
 	const fullFile = ['#!/bin/sh', ...content, 'exec "$@"'].join('\n');
