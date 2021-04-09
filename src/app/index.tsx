46e0d8b8237d6cbf05e14475782bc3202509ad39
Joseph Garrone <joseph.garrone.gj@gmail.com>
2021-04-09 06:19:40 +0200

    Fix logic on register page

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -34,8 +34,8 @@ Object.defineProperty(
 );
 
 const kcContext = realKcContext ?? (
-    false /* Set to true to test the login pages outside of Keycloak */
-        ? kcContextMocks.kcLoginContext /* Change to .kcRegisterContext for example */
+    true /* Set to true to test the login pages outside of Keycloak */
+        ? kcContextMocks.kcRegisterContext /* Change to .kcRegisterContext for example */
         :
         undefined
 );
