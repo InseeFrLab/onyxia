b39aba7b52818e569d5371cc132b9bba39840d83
garronej <joseph.garrone.gj@gmail.com>
2021-05-06 20:23:51 +0200

    Add instructions about how to import assets

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,8 +1,4 @@
 /// <reference types="react-scripts" />
-declare module '*.mp4' {
-    const src: string;
-    export default src;
-}
 declare module '*.md' {
     const src: string;
     export default src;
