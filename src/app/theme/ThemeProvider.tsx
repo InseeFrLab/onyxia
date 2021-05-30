4a3e742e1a789747f59cb8c47ba5dd6ca96a512b
garronej <joseph.garrone.gj@gmail.com>
2021-05-30 04:37:12 +0200

    Externalizing design: Isolate zoom provider from theme provider

--- src/app/theme/ThemeProvider.tsx
+++ src/app/theme/ThemeProvider.tsx
@@ -1,13 +1,8 @@
 
 import { useMemo } from "react";
 import CssBaseline from '@material-ui/core/CssBaseline';
-import { ZoomProvider } from "powerhooks";
-import { useWindowInnerSize } from "powerhooks";
-
 import { ThemeProvider as MuiThemeProvider, StylesProvider } from "@material-ui/core/styles";
 import { createTheme } from "./theme";
-
-import { useEffectOnValueChange } from "powerhooks";
 
 export function themeProviderFactory(
     params: {
@@ -20,61 +15,31 @@ export function themeProviderFactory(
     function ThemeProvider(
         props: {
             isDarkModeEnabled: boolean;
-            /** set to undefined to disable */
-            zoomProviderReferenceWidth?: number;
             children: React.ReactNode;
         }
     ) {
 
         const {
             isDarkModeEnabled,
-            zoomProviderReferenceWidth,
             children
         } = props;
 
         const { theme } = useMemo(
             () => createTheme({
-                    isReactStrictModeEnabled,
-                    isDarkModeEnabled
-                }),
+                isReactStrictModeEnabled,
+                isDarkModeEnabled
+            }),
             [isDarkModeEnabled]
-        );
-
-        const { windowInnerHeight, windowInnerWidth } = useWindowInnerSize();
-
-        const isLandscape = windowInnerWidth > windowInnerHeight;
-
-        useEffectOnValueChange(
-            () => {
-
-                if (zoomProviderReferenceWidth === undefined) {
-                    return;
-                }
-
-                window.location.reload();
-            },
-            [isLandscape]
         );
 
         return (
             <MuiThemeProvider theme={theme}>
                 <CssBaseline />
                 <StylesProvider injectFirst>
-                    {
-                        zoomProviderReferenceWidth !== undefined && !isLandscape ?
-                            <p>
-                                This app isn't compatible with landscape mode yet,
-                                please enable the rotation sensor and flip your phone.
-                            </p>
-                            :
-                            <ZoomProvider referenceWidth={zoomProviderReferenceWidth}>
-                                {children}
-                            </ZoomProvider>
-                    }
+                    {children}
                 </StylesProvider>
             </MuiThemeProvider>
         );
-
 
     }
 
