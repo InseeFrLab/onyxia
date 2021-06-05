a116e6a00cbe6a53c539da3a1dc2a764445648f6
garronej <joseph.garrone.gj@gmail.com>
2021-06-05 17:59:09 +0200

    Refactor the way we instantiate the store

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,25 +1,20 @@
 import { StrictMode } from "react";
 import * as reactDom from "react-dom";
-import { getValidatedEnv } from "./validatedEnv";
-import memoize from "memoizee";
-
-import type { OidcClientConfig, SecretsManagerClientConfig, OnyxiaApiClientConfig } from "lib/setup";
-import { id } from "tsafe/id";
 import { I18nProvider } from "./i18n/I18nProvider";
 import { RouteProvider } from "./routes/router";
-import { StoreProvider } from "app/interfaceWithLib/StoreProvider";
-import type { Props as StoreProviderProps } from "app/interfaceWithLib/StoreProvider";
+import { createStoreProvider } from "app/interfaceWithLib/StoreProvider";
 import { ThemeProvider } from "./theme";
 import { SplashScreenProvider } from "onyxia-ui";
 import { App } from "app/components/App";
-import { useLng } from "app/i18n/useLng";
 import {
     kcContext as realKcContext,
     kcContextMocks
 } from "keycloakify";
 import { KcApp } from "app/components/KcApp";
 import { ReactComponent as OnyxiaLogoSvg } from "app/assets/svg/OnyxiaLogo.svg";
-import { Typography } from "onyxia-ui";
+import { Typography } from "onyxia-ui";
+
+const { StoreProvider } = createStoreProvider({ "doMock": false });
 
 const kcContext = realKcContext ?? (
     false /* Set to true to test the login pages outside of Keycloak */
@@ -28,81 +23,25 @@ const kcContext = realKcContext ?? (
         undefined
 );
 
-const getStoreInitializationParams = memoize(
-    id<StoreProviderProps["getStoreInitializationParams"]>(() => {
-
-        const env = getValidatedEnv();
-
-        return {
-            "oidcClientConfig":
-                env.AUTHENTICATION.TYPE === "oidc" ?
-                    id<OidcClientConfig.Keycloak>({
-                        "implementation": "KEYCLOAK",
-                        "keycloakConfig": env.AUTHENTICATION.OIDC
-                    }) :
-                    id<OidcClientConfig.Phony>({
-                        "implementation": "PHONY",
-                        "tokenValidityDurationMs": Infinity,
-                        "parsedJwt": {
-                            "email": "john.doe@insee.fr",
-                            "preferred_username": "jdoe",
-                            "family_name": "Doe",
-                            "given_name": "John",
-                            "groups": [],
-                            "locale": "fr"
-                        }
-                    }),
-            "secretsManagerClientConfig": id<SecretsManagerClientConfig.Vault>({
-                "implementation": "VAULT",
-                "baseUri": env.VAULT.BASE_URI,
-                "engine": env.VAULT.ENGINE,
-                "role": env.VAULT.ROLE
-            }),
-            "onyxiaApiClientConfig": id<OnyxiaApiClientConfig.Official>({
-                "implementation": "OFFICIAL",
-                "baseUrl": env.API.BASE_URL ?? (() => {
-
-                    const { protocol, host } = window.location;
-
-                    return `${protocol}//${host}/api`;
-
-                })()
-            })
-        };
-
-    })
-);
-
-
-function Root() {
-
-    const { lng } = useLng();
-
-    return (
-        <StrictMode>
-            <I18nProvider lng={lng}>
-                <RouteProvider>
-                    <ThemeProvider
-                        zoomProviderReferenceWidth={kcContext !== undefined ? undefined : 1920}
-                        portraitModeUnsupportedMessage={<Typography variant="h3">Please rotate your phone</Typography>}
-                    >
-                        <SplashScreenProvider Logo={OnyxiaLogoSvg}>
-                            {kcContext !== undefined ?
-                                <KcApp kcContext={kcContext} /> :
-                                <StoreProvider getStoreInitializationParams={getStoreInitializationParams}>
-                                    <App />
-                                </StoreProvider>
-                            }
-                        </SplashScreenProvider>
-                    </ThemeProvider>
-                </RouteProvider>
-            </I18nProvider>
-        </StrictMode>
-    );
-
-}
-
 reactDom.render(
-    <Root />,
+    <StrictMode>
+        <I18nProvider>
+            <RouteProvider>
+                <ThemeProvider
+                    zoomProviderReferenceWidth={kcContext !== undefined ? undefined : 1920}
+                    portraitModeUnsupportedMessage={<Typography variant="h3">Please rotate your phone</Typography>}
+                >
+                    <SplashScreenProvider Logo={OnyxiaLogoSvg}>
+                        {kcContext !== undefined ?
+                            <KcApp kcContext={kcContext} /> :
+                            <StoreProvider>
+                                <App />
+                            </StoreProvider>
+                        }
+                    </SplashScreenProvider>
+                </ThemeProvider>
+            </RouteProvider>
+        </I18nProvider>
+    </StrictMode>,
     document.getElementById("root")
 );
--- src/app/interfaceWithLib/StoreProvider.tsx
+++ src/app/interfaceWithLib/StoreProvider.tsx
@@ -3,65 +3,132 @@ import type { ReactNode } from "react";
 import { Provider as ReactReduxProvider } from "react-redux";
 import { useAsync } from "react-async-hook";
 import { createStore } from "lib/setup";
-import type { CreateStoreParams } from "lib/setup";
 import { getIsDarkModeEnabledOsDefault } from "onyxia-ui";
-import memoize from "memoizee";
+import memoize from "memoizee";
+import { getValidatedEnv } from "app/validatedEnv";
+import { id } from "tsafe/id";
+import type { OidcClientConfig, SecretsManagerClientConfig, OnyxiaApiClientConfig } from "lib/setup";
+import * as mockData from "./mockData";
 
-export type StoreInitializationParams = Omit<
-        CreateStoreParams,
-        "getIsDarkModeEnabledValueForProfileInitialization"
-    >;
-
-
-export type Props = {
-    /** 
-     * We use a getter instead of just an object to enable 
-     * lazy evaluation. 
-     * The getter will be called ONLY ONCE.
-     * You can't change the store parameters after it has been
-     * first initialized. Swiping the reference of this getter will
-     * have no effect.
-     */
-    getStoreInitializationParams(): StoreInitializationParams;
-    children: ReactNode;
-};
-
-//NOTE: Create store can only be called once
-const createStore_memo = memoize(
-    (storeInitializationParams: StoreInitializationParams)=> 
-        createStore({
-            ...storeInitializationParams,
-            "getIsDarkModeEnabledValueForProfileInitialization": getIsDarkModeEnabledOsDefault
-        }),
-    { "promise": true }
-);
-
-export function StoreProvider(props: Props) {
-
-    const { getStoreInitializationParams, children } = props;
-
-    const asyncCreateStore = useAsync(
-        ()=> createStore_memo(getStoreInitializationParams()),
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-        []
-    );
-
-    if (asyncCreateStore.error) {
-        throw asyncCreateStore.error;
+export function createStoreProvider(
+    params: {
+        doMock: boolean;
     }
+) {
+    const { doMock } = params;
 
-    const { result: store } = asyncCreateStore;
+    //NOTE: Create store can only be called once
+    const createStore_memo = memoize(
+        () => {
 
-    return (
-        store === undefined ?
-            null :
-            <ReactReduxProvider store={store}>
-                {children}
-            </ReactReduxProvider>
+            const env = getValidatedEnv();
+
+            return createStore(
+                {
+                    "oidcClientConfig":
+                        doMock ?
+                            id<OidcClientConfig.Phony>({
+                                "implementation": "PHONY",
+                                "tokenValidityDurationMs": Infinity,
+                                "parsedJwt": {
+                                    "email": "john.doe@insee.fr",
+                                    "preferred_username": "jdoe",
+                                    "family_name": "Doe",
+                                    "given_name": "John",
+                                    "groups": ["sspcloud-admin", "dsi-ddc"],
+                                    "locale": "en"
+                                }
+                            }) :
+                            env.AUTHENTICATION.TYPE === "oidc" ?
+                                id<OidcClientConfig.Keycloak>({
+                                    "implementation": "KEYCLOAK",
+                                    "keycloakConfig": env.AUTHENTICATION.OIDC
+                                }) :
+                                id<OidcClientConfig.Phony>({
+                                    "implementation": "PHONY",
+                                    "tokenValidityDurationMs": Infinity,
+                                    "parsedJwt": {
+                                        "email": "john.doe@insee.fr",
+                                        "preferred_username": "jdoe",
+                                        "family_name": "Doe",
+                                        "given_name": "John",
+                                        "groups": [],
+                                        "locale": "fr"
+                                    }
+                                }),
+                    "secretsManagerClientConfig":
+                        doMock ?
+                            id<SecretsManagerClientConfig.LocalStorage>({
+                                "implementation": "LOCAL STORAGE",
+                                "artificialDelayMs": 0,
+                                "doReset": false,
+                                "paramsForTranslator": {
+                                    "baseUri": "https://vault.lab.sspcloud.fr",
+                                    "engine": "onyxia-kv",
+                                    "role": "onyxia-user"
+                                }
+                            }) :
+                            id<SecretsManagerClientConfig.Vault>({
+                                "implementation": "VAULT",
+                                "baseUri": env.VAULT.BASE_URI,
+                                "engine": env.VAULT.ENGINE,
+                                "role": env.VAULT.ROLE
+                            }),
+                    "onyxiaApiClientConfig":
+                        doMock ?
+                            id<OnyxiaApiClientConfig.Mock>({
+                                "implementation": "MOCK",
+                                ...mockData
+                            }) :
+                            id<OnyxiaApiClientConfig.Official>({
+                                "implementation": "OFFICIAL",
+                                "baseUrl": env.API.BASE_URL ?? (() => {
+
+                                    const { protocol, host } = window.location;
+
+                                    return `${protocol}//${host}/api`;
+
+                                })()
+                            }),
+                    "getIsDarkModeEnabledValueForProfileInitialization": getIsDarkModeEnabledOsDefault
+                }
+            );
+        },
+        { "promise": true }
     );
 
-};
+    type Props = {
+        children: ReactNode;
+    };
 
+    function StoreProvider(props: Props) {
 
+        const { children } = props;
+
+        const asyncCreateStore = useAsync(
+            () => createStore_memo(),
+            // eslint-disable-next-line react-hooks/exhaustive-deps
+            []
+        );
+
+        if (asyncCreateStore.error) {
+            throw asyncCreateStore.error;
+        }
+
+        const { result: store } = asyncCreateStore;
+
+        return (
+            store === undefined ?
+                null :
+                <ReactReduxProvider store={store}>
+                    {children}
+                </ReactReduxProvider>
+        );
+
+    };
+
+    return { StoreProvider };
+
+}
 
 
