ee21950983d2cf0de34bc1d36fc403914a3bc927
garronej <joseph.garrone.gj@gmail.com>
2021-09-22 16:37:20 +0200

    Fix previous build

--- src/app/interfaceWithLib/StoreProvider.tsx
+++ src/app/interfaceWithLib/StoreProvider.tsx
@@ -12,9 +12,12 @@ import type {
 } from "lib/setup";
 import { getEnv } from "env";
 import * as mockData from "./mockData";
+import { validateEnvs } from "validateEnv";
 
 export function createStoreProvider(params: { isStorybook: boolean }) {
     const { isStorybook } = params;
+
+    validateEnvs();
 
     //NOTE: Create store can only be called once
     const createStore_memo = memoize(
