e0057d340c2768b1d6adefe6c227800d306e497b
garronej <joseph.garrone.gj@gmail.com>
2021-11-08 20:21:09 +0100

    Login: Improve UX on new register page

--- src/app/components/KcApp/kcContext.ts
+++ src/app/components/KcApp/kcContext.ts
@@ -9,7 +9,7 @@ export const { kcContext } = getKcContext<{
      */
     authorizedMailDomains?: string[];
 }>({
-    //"mockPageId": "login.ftl",
+    "mockPageId": "register-user-profile.ftl",
     /**
      * Customize the simulated kcContext that will let us
      * dev the page outside keycloak (with auto-reload)
@@ -83,6 +83,34 @@ export const { kcContext } = getKcContext<{
                 "registrationEmailAsUsername": false,
             },
             /* spell-checker: enable */
+        },
+        {
+            "pageId": "register-user-profile.ftl",
+            "profile": {
+                "attributes": [
+                    {
+                        "validators": {
+                            "pattern": {
+                                "pattern": "^[a-zA-Z0-9]+$",
+                                "ignore.empty.value": true,
+                            },
+                        },
+                        "value": undefined,
+                        "name": "username",
+                    },
+                    {
+                        "validators": {
+                            "pattern": {
+                                /* spell-checker: disable */
+                                "pattern":
+                                    "@([^.]+.)?((insee.fr)|(gouv.fr)|(ensae.fr)|(ensai.fr)|(ign.fr)|(has-sante.fr)|(casd.eu)|(ars.sante.fr)|(cnaf.fr)|(ac-lille.fr)|(ac-amiens.fr)|(ac-normandie.fr)|(ac-reims.fr)|(ac-nancy-metz.fr)|(ac-strasbourg.fr)|(ac-creteil.fr)|(ac-paris.fr)|(ac-versailles.fr)|(ac-rennes.fr)|(ac-nantes.fr)|(ac-orleans-tours.fr)|(ac-dijon.fr)|(ac-besancon.fr)|(ac-poitiers.fr)|(ac-limoges.fr)|(ac-clermont.fr)|(ac-lyon.fr)|(ac-grenoble.fr)|(ac-bordeaux.fr)|(ac-toulouse.fr)|(ac-montpellier.fr)|(ac-aix-marseille.fr)|(ac-nice.fr)|(ac-corse.fr)|(ac-martinique.fr)|(ac-guadeloupe.fr)|(ac-reunion.fr)|(ac-guyane.fr)|(ac-mayotte.fr)|(ac-wf.wf)|(monvr.pf)|(ac-noumea.nc)|(ac-spm.fr))$",
+                                /* spell-checker: enabled */
+                            },
+                        },
+                        "name": "email",
+                    },
+                ],
+            },
         },
     ],
 });
