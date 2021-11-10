95fc737d772c49ad052209fe9b8d94c76a3ec010
garronej <joseph.garrone.gj@gmail.com>
2021-11-10 19:51:51 +0100

    Fix email regular expression in mock kcContext

--- src/app/components/KcApp/kcContext.ts
+++ src/app/components/KcApp/kcContext.ts
@@ -103,7 +103,7 @@ export const { kcContext } = getKcContext<{
                             "pattern": {
                                 /* spell-checker: disable */
                                 "pattern":
-                                    "@([^.]+.)?((insee.fr)|(gouv.fr)|(ensae.fr)|(ensai.fr)|(ign.fr)|(has-sante.fr)|(casd.eu)|(ars.sante.fr)|(cnaf.fr)|(ac-lille.fr)|(ac-amiens.fr)|(ac-normandie.fr)|(ac-reims.fr)|(ac-nancy-metz.fr)|(ac-strasbourg.fr)|(ac-creteil.fr)|(ac-paris.fr)|(ac-versailles.fr)|(ac-rennes.fr)|(ac-nantes.fr)|(ac-orleans-tours.fr)|(ac-dijon.fr)|(ac-besancon.fr)|(ac-poitiers.fr)|(ac-limoges.fr)|(ac-clermont.fr)|(ac-lyon.fr)|(ac-grenoble.fr)|(ac-bordeaux.fr)|(ac-toulouse.fr)|(ac-montpellier.fr)|(ac-aix-marseille.fr)|(ac-nice.fr)|(ac-corse.fr)|(ac-martinique.fr)|(ac-guadeloupe.fr)|(ac-reunion.fr)|(ac-guyane.fr)|(ac-mayotte.fr)|(ac-wf.wf)|(monvr.pf)|(ac-noumea.nc)|(ac-spm.fr))$",
+                                    "^[^@]+@([^.]+.)?((insee.fr)|(gouv.fr)|(ensae.fr)|(ensai.fr)|(ign.fr)|(has-sante.fr)|(casd.eu)|(ars.sante.fr)|(cnaf.fr)|(ac-lille.fr)|(ac-amiens.fr)|(ac-normandie.fr)|(ac-reims.fr)|(ac-nancy-metz.fr)|(ac-strasbourg.fr)|(ac-creteil.fr)|(ac-paris.fr)|(ac-versailles.fr)|(ac-rennes.fr)|(ac-nantes.fr)|(ac-orleans-tours.fr)|(ac-dijon.fr)|(ac-besancon.fr)|(ac-poitiers.fr)|(ac-limoges.fr)|(ac-clermont.fr)|(ac-lyon.fr)|(ac-grenoble.fr)|(ac-bordeaux.fr)|(ac-toulouse.fr)|(ac-montpellier.fr)|(ac-aix-marseille.fr)|(ac-nice.fr)|(ac-corse.fr)|(ac-martinique.fr)|(ac-guadeloupe.fr)|(ac-reunion.fr)|(ac-guyane.fr)|(ac-mayotte.fr)|(ac-wf.wf)|(monvr.pf)|(ac-noumea.nc)|(ac-spm.fr))$",
                                 /* spell-checker: enabled */
                             },
                         },
