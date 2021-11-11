4be0923399b4ce7db1282179a674e33f19b92419
garronej <joseph.garrone.gj@gmail.com>
2021-11-11 22:31:37 +0100

    Remove old register page, force the use of Keycloak user attribute

--- src/app/components/KcApp/kcContext.ts
+++ src/app/components/KcApp/kcContext.ts
@@ -5,14 +5,7 @@ import type { KcLanguageTag } from "keycloakify";
 import { id } from "tsafe/id";
 
 /** It's the Keycloak context, it is undefined unless we are on Keycloak, rendering the login pages. */
-export const { kcContext } = getKcContext<{
-    pageId: "register.ftl";
-    /**
-     * Defined when you use the keycloak-mail-whitelisting keycloak plugin
-     * (https://github.com/micedre/keycloak-mail-whitelisting)
-     */
-    authorizedMailDomains?: string[];
-}>({
+export const { kcContext } = getKcContext({
     //"mockPageId": "register-user-profile.ftl",
     /**
      * Customize the simulated kcContext that will let us
@@ -31,62 +24,6 @@ export const { kcContext } = getKcContext<{
                     },
                 ],
             },
-        },
-        {
-            "pageId": "register.ftl",
-            /* spell-checker: disable */
-            "authorizedMailDomains": [
-                "insee.fr",
-                "gouv.fr",
-                "casd.eu",
-                "ensai.fr",
-                "ensae.fr",
-                "ars.sante.fr",
-                "*.gouv.fr",
-                "*.sante.gouv.fr",
-                "sante.gouv.fr",
-                "cnaf.fr",
-                "*.cnaf.fr",
-                "ac-lille.fr",
-                "ac-amiens.fr",
-                "ac-normandie.fr",
-                "ac-reims.fr",
-                "ac-nancy-metz.fr",
-                "ac-strasbourg.fr",
-                "ac-creteil.fr",
-                "ac-paris.fr",
-                "ac-versailles.fr",
-                "ac-rennes.fr",
-                "ac-nantes.fr",
-                "ac-orleans-tours.fr",
-                "ac-dijon.fr",
-                "ac-besancon.fr",
-                "ac-poitiers.fr",
-                "ac-limoges.fr",
-                "ac-clermont.fr",
-                "ac-lyon.fr",
-                "ac-grenoble.fr",
-                "ac-bordeaux.fr",
-                "ac-toulouse.fr",
-                "ac-montpellier.fr",
-                "ac-aix-marseille.fr",
-                "ac-nice.fr",
-                "ac-corse.fr",
-                "ac-martinique.fr",
-                "ac-guadeloupe.fr",
-                "ac-reunion.fr",
-                "ac-guyane.fr",
-                "ac-mayotte.fr",
-                "ac-wf.wf",
-                "monvr.pf",
-                "ac-noumea.nc",
-                "ac-spm.fr",
-                "*.ensai.fr",
-            ],
-            "realm": {
-                "registrationEmailAsUsername": false,
-            },
-            /* spell-checker: enable */
         },
         {
             "pageId": "register-user-profile.ftl",
