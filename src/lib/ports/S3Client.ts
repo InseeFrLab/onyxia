6dbe28aee8fc728a29c162d449a40d16c06d1590
garronej <joseph.garrone.gj@gmail.com>
2021-11-17 00:16:53 +0100

    Implement list function for minio adapter

--- src/lib/ports/S3Client.ts
+++ src/lib/ports/S3Client.ts
@@ -1,4 +1,11 @@
 export type S3Client = {
+    getFsApi: (params: { bucketName: string }) => {
+        list: (params: { path: string }) => Promise<{
+            directories: string[];
+            files: string[];
+        }>;
+    };
+
     getToken: () => Promise<{
         accessKeyId: string;
         secretAccessKey: string;
