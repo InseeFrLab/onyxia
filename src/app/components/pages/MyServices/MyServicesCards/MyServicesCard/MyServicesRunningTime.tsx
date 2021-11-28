b704f85ed0e09887a2101e770e1a95c2f45ba1bc
garronej <joseph.garrone.gj@gmail.com>
2021-11-28 11:23:20 +0100

    Add missing CSS classes labels

--- src/app/components/pages/MyServices/MyServicesCards/MyServicesCard/MyServicesRunningTime.tsx
+++ src/app/components/pages/MyServices/MyServicesCards/MyServicesCard/MyServicesRunningTime.tsx
@@ -4,21 +4,6 @@ import { makeStyles, Icon, Text } from "app/theme";
 import { useFromNow } from "app/i18n/useMoment";
 import { useTranslation } from "app/i18n/useTranslations";
 
-const useStyles = makeStyles<{ isOvertime: boolean }>()((theme, { isOvertime }) => {
-    const color = isOvertime ? theme.colors.useCases.alertSeverity.error.main : undefined;
-
-    return {
-        "root": {
-            color,
-            "display": "flex",
-            "alignItems": "center",
-        },
-        "icon": {
-            color,
-        },
-    };
-});
-
 export type Props = {
     className?: string;
 } & (
@@ -58,3 +43,20 @@ export declare namespace MyServicesRunningTime {
         "launching": undefined;
     };
 }
+
+const useStyles = makeStyles<{ isOvertime: boolean }>({
+    "label": { MyServicesRunningTime },
+})((theme, { isOvertime }) => {
+    const color = isOvertime ? theme.colors.useCases.alertSeverity.error.main : undefined;
+
+    return {
+        "root": {
+            color,
+            "display": "flex",
+            "alignItems": "center",
+        },
+        "icon": {
+            color,
+        },
+    };
+});
