a561672789c6f553370d6b24bf2848e85eb0a5be
garronej <joseph.garrone.gj@gmail.com>
2022-03-14 17:15:17 +0100

    Use arrow function notation in types

--- src/core/ports/OidcClient.ts
+++ src/core/ports/OidcClient.ts
@@ -3,12 +3,12 @@ export declare type OidcClient = OidcClient.LoggedIn | OidcClient.NotLoggedIn;
 export declare namespace OidcClient {
     export type NotLoggedIn = {
         isUserLoggedIn: false;
-        login(): Promise<never>;
+        login: () => Promise<never>;
     };
 
     export type LoggedIn = {
         isUserLoggedIn: true;
-        getAccessToken(): Promise<string>;
+        getAccessToken: () => Promise<string>;
         logout: (params: { redirectTo: "home" | "current page" }) => Promise<never>;
     };
 }
