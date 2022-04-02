abad2b54562957d4affb5cd408fb140c48de3ce0
garronej <joseph.garrone.gj@gmail.com>
2022-04-02 18:01:18 +0200

    Use css shadow instead of svg

--- src/ui/components/pages/MyFilesMySecrets/Explorer/ExplorerIcon.tsx
+++ src/ui/components/pages/MyFilesMySecrets/Explorer/ExplorerIcon.tsx
@@ -1,9 +1,5 @@
 import { makeStyles } from "ui/theme";
 import { useMemo, memo } from "react";
-import { useDomRect } from "powerhooks/useDomRect";
-import { ReactComponent as SvgDataWithShadow } from "ui/assets/svg/explorer/dataWithShadow.svg";
-import { ReactComponent as SvgSecretWithShadow } from "ui/assets/svg/explorer/secretWithShadow.svg";
-import { ReactComponent as SvgDirectoryWithShadow } from "ui/assets/svg/explorer/directoryWithShadow.svg";
 import { ReactComponent as SvgData } from "ui/assets/svg/explorer/data.svg";
 import { ReactComponent as SvgSecret } from "ui/assets/svg/explorer/secret.svg";
 import { ReactComponent as SvgDirectory } from "ui/assets/svg/explorer/directory.svg";
@@ -11,7 +7,6 @@ import { ReactComponent as SvgDirectory } from "ui/assets/svg/explorer/directory
 //Figma -> Inkscape -> https://svg2jsx.com/
 
 export type Props = {
-    /** A height (and no with) must be provided explicitly */
     className?: string;
     iconId: "data" | "secret" | "directory";
     hasShadow: boolean;
@@ -20,51 +15,27 @@ export type Props = {
 export const ExplorerIcon = memo((props: Props) => {
     const { className, iconId, hasShadow } = props;
 
-    const {
-        ref,
-        domRect: { height },
-    } = useDomRect();
-
-    const { cx, classes } = useStyles({ height, iconId, hasShadow });
+    const { cx, classes } = useStyles({ hasShadow, iconId });
 
     const SvgReactComponent = useMemo(() => {
         switch (iconId) {
             case "data":
-                return hasShadow ? SvgDataWithShadow : SvgData;
+                return SvgData;
             case "secret":
-                return hasShadow ? SvgSecretWithShadow : SvgSecret;
+                return SvgSecret;
             case "directory":
-                return hasShadow ? SvgDirectoryWithShadow : SvgDirectory;
+                return SvgDirectory;
         }
     }, [iconId, hasShadow]);
 
-    return (
-        <div ref={ref} className={cx(classes.root, className)}>
-            <SvgReactComponent className={classes.svg} />
-        </div>
-    );
+    return <SvgReactComponent className={cx(classes.root, className)} />;
 });
 
-const useStyles = makeStyles<{ height: number } & Pick<Props, "iconId" | "hasShadow">>({
+const useStyles = makeStyles<Pick<Props, "hasShadow" | "iconId">>({
     "name": { ExplorerIcon },
-})((theme, { height, iconId, hasShadow }) => ({
+})((theme, { hasShadow, iconId }) => ({
     "root": {
-        height,
-        "width":
-            (() => {
-                switch (iconId) {
-                    case "data":
-                        return hasShadow ? 0.88 : 0.87;
-                    case "secret":
-                        return 0.87;
-                    case "directory":
-                        return 1.26;
-                }
-            })() * height,
-        "position": "relative",
-    },
-    "svg": {
-        "position": "absolute",
+        "filter": hasShadow ? "drop-shadow(0px 3px 3px rgba(0, 0, 0, 0.4))" : undefined,
         "fill": "currentColor",
         "color": (() => {
             switch (iconId) {
@@ -76,47 +47,5 @@ const useStyles = makeStyles<{ height: number } & Pick<Props, "iconId" | "hasSha
                     ].main;
             }
         })(),
-        "height":
-            height *
-            (!hasShadow
-                ? 1
-                : (() => {
-                      switch (iconId) {
-                          case "data":
-                              return 1.31;
-                          case "secret":
-                              return 1.12;
-                          case "directory":
-                              return 1.185;
-                      }
-                  })()),
-        "top": !hasShadow
-            ? 0
-            : -(
-                  (() => {
-                      switch (iconId) {
-                          case "data":
-                              return 4;
-                          case "secret":
-                              return 1;
-                          case "directory":
-                              return 2;
-                      }
-                  })() / 100
-              ) * height,
-        "left": !hasShadow
-            ? 0
-            : -(
-                  (() => {
-                      switch (iconId) {
-                          case "data":
-                              return 10;
-                          case "secret":
-                              return 6;
-                          case "directory":
-                              return 4;
-                      }
-                  })() / 100
-              ) * height,
     },
 }));
