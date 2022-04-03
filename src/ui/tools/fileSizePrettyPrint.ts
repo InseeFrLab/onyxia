3cead2718d6f031b313b1614a19651cec78a582e
garronej <joseph.garrone.gj@gmail.com>
2022-04-03 02:20:28 +0200

    Clean fixed size for ExplorerUploadProgress

--- src/ui/tools/fileSizePrettyPrint.ts
+++ src/ui/tools/fileSizePrettyPrint.ts
@@ -2,24 +2,70 @@ const thresh = 1000;
 /** @param dp Number of decimal places to display. */
 const dp = 1;
 
-export function fileSizePrettyPrint(bytes: number) {
-    if (Math.abs(bytes) < thresh) {
-        return bytes + " B";
-    }
+const units = ["kB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"] as const;
 
-    const units = ["kB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+type Unit = "B" | typeof units[number];
+
+export function fileSizePrettyPrint(params: { bytes: number; unit?: Unit }): {
+    value: string;
+    unit: Unit;
+} {
+    let { bytes } = params;
+
+    if (Math.abs(bytes) < thresh || params.unit === "B") {
+        return {
+            "value": `${bytes}`,
+            "unit": "B",
+        };
+    }
 
     let u = -1;
     const r = 10 ** dp;
 
+    let targetU = params.unit === undefined ? undefined : units.indexOf(params.unit);
+
     do {
         bytes /= thresh;
         ++u;
-    } while (Math.round(Math.abs(bytes) * r) / r >= thresh && u < units.length - 1);
+    } while (
+        (targetU !== undefined && targetU > u) ||
+        (Math.round(Math.abs(bytes) * r) / r >= thresh && u < units.length - 1)
+    );
 
     const unit = units[u];
 
-    return `${bytes.toFixed(dp)} ${unit}`;
+    return {
+        "value": bytes.toFixed(dp),
+        unit,
+    };
 }
 
-console.log(fileSizePrettyPrint(200));
+/*
+console.log(
+fileSizePrettyPrint({
+    "bytes": 2_000,
+    "unit": "B"
+})
+);
+
+console.log(
+fileSizePrettyPrint({
+    "bytes": 2_000,
+    "unit": "kB"
+})
+);
+
+console.log(
+fileSizePrettyPrint({
+    "bytes": 100_000,
+    "unit": "MB"
+})
+);
+
+console.log(
+fileSizePrettyPrint({
+    "bytes": 100_000,
+    "unit": "GB"
+})
+);
+*/
