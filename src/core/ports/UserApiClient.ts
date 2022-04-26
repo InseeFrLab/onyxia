60db0563e6c31a6f67add0340e857d264697117f
garronej <joseph.garrone.gj@gmail.com>
2022-04-26 16:18:20 +0200

    JWT_FAMILY_NAME and JWT_FIRST_NAME are optional

--- src/core/ports/UserApiClient.ts
+++ src/core/ports/UserApiClient.ts
@@ -1,7 +1,7 @@
 export type User = {
     email: string;
-    familyName: string;
-    firstName: string;
+    familyName?: string;
+    firstName?: string;
     username: string;
     groups?: string[];
     locale?: string;
