77c5b8d86d822e7dccd64a6eb7130c21b7e380bd
garronej <joseph.garrone.gj@gmail.com>
2022-07-09 04:17:03 +0200

    Be resilient if the path of overwriteDefaultWith dosen't exsits

--- src/core/tools/getValueAtPathInObject.ts
+++ src/core/tools/getValueAtPathInObject.ts
@@ -1,23 +1,31 @@
-import { assert } from "tsafe/assert";
-
-export function getValueAtPathInObject(params: {
+/** NOTE: It returns undefined if the path is not found */
+export function getValueAtPathInObject<T>(params: {
     path: string[];
     obj: Record<string, unknown>;
-}): any {
+}): T | undefined {
+    return getValueAtPathInObjectRec(params);
+}
+
+function getValueAtPathInObjectRec<T>(params: {
+    path: string[];
+    obj: any;
+}): T | undefined {
     const { path, obj } = params;
 
     if (path.length === 0) {
         return obj;
     }
 
+    if (obj instanceof Object) {
+        return undefined;
+    }
+
     const [key, ...newPath] = path;
 
     const newObj = obj[key];
 
-    assert(newObj instanceof Object, `can't dereference ${key}`);
-
-    return getValueAtPathInObject({
+    return getValueAtPathInObjectRec({
         "path": newPath,
-        "obj": newObj as Record<string, unknown>,
+        "obj": newObj,
     });
 }
