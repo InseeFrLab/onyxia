251de68ac384a6fee56690fb1aaa44e841236fe7
Dylan Decrulle <81740200+ddecrulle@users.noreply.github.com>
2022-09-12 14:49:23 +0200

    webpack5 path compatibility

--- src/core/secondaryAdapters/localStorageSecretsManagerClient.ts
+++ src/core/secondaryAdapters/localStorageSecretsManagerClient.ts
@@ -1,4 +1,4 @@
-import { join as pathJoin, relative as pathRelative } from "core/tools/path";
+import { join as pathJoin, relative as pathRelative } from "path";
 import { partition } from "evt/tools/reducers/partition";
 import { removeDuplicates } from "evt/tools/reducers/removeDuplicates";
 import { SecretWithMetadata, SecretsManagerClient } from "../ports/SecretsManagerClient";
