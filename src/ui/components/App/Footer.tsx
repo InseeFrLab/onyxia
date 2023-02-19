9f0656b11f88266e7e17cef06568145d3c58c4ab
garronej <joseph.garrone@data.gouv.fr>
2023-02-19 01:12:06 +0100

    Update Footer

--- src/ui/components/App/Footer.tsx
+++ src/ui/components/App/Footer.tsx
@@ -27,7 +27,7 @@ export const Footer = memo((props: Props) => {
 
     return (
         <footer className={cx(classes.root, className)}>
-            <Text typo="body 2">2017 - 2022 Onyxia, InseefrLab</Text>
+            <Text typo="body 2">2017 - 2023 Onyxia, INSEE, CodeGouv</Text>
             {spacing}
             <a
                 href={contributeUrl}
