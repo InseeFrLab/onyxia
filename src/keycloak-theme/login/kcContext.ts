5b6e59416fdfcb05ece366f0f6199c0ac1284f77
garronej <joseph.garrone@data.gouv.fr>
2023-05-02 09:31:03 +0200

    Migrate to Keycloakify 7 p5

--- src/keycloak-theme/login/kcContext.ts
+++ src/keycloak-theme/login/kcContext.ts
@@ -50,7 +50,7 @@ export const { getKcContext } = createGetKcContext({
 
 export const { kcContext } = getKcContext({
     // Uncomment to test the login page for development.
-    //mockPageId: "login.ftl",
+    //"mockPageId": "login.ftl",
 });
 
 export type KcContext = NonNullable<ReturnType<typeof getKcContext>["kcContext"]>;
