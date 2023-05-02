d58fdcba7725a5cc72d647860babd52a5f97db00
garronej <joseph.garrone@data.gouv.fr>
2023-05-02 06:13:55 +0200

    Migrate to Keycloakify 7 p3

--- src/keycloak-theme/login/pages/shared/generateUsername.ts
+++
@@ -1,9 +0,0 @@
-export function toAlphaNumerical(value: string) {
-    return value.replace(/[^a-zA-Z0-9]/g, "x");
-}
-
-export function generateUsername(params: { firstName: string; lastName: string }) {
-    const { firstName, lastName } = params;
-
-    return toAlphaNumerical(`${firstName[0] ?? ""}${lastName}`).toLowerCase();
-}
