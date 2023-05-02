12617da09e65468ba62fbced8c8176beeb64d299
garronej <joseph.garrone@data.gouv.fr>
2023-05-02 10:08:57 +0200

    Remove the keycloak account theme for now

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,10 @@
 import { lazy, Suspense } from "react";
 import { createRoot } from "react-dom/client";
 import { kcContext as kcLoginThemeContext } from "keycloak-theme/login/kcContext";
-import { kcContext as kcAccountThemeContext } from "keycloak-theme/account/kcContext";
 import { assert } from "tsafe/assert";
 
 const App = lazy(() => import("ui/App"));
 const KcLoginThemeApp = lazy(() => import("keycloak-theme/login/KcApp"));
-const KcAccountThemeApp = lazy(() => import("keycloak-theme/account/KcApp"));
 
 createRoot(
     (() => {
@@ -21,10 +19,6 @@ createRoot(
         {(() => {
             if (kcLoginThemeContext !== undefined) {
                 return <KcLoginThemeApp kcContext={kcLoginThemeContext} />;
-            }
-
-            if (kcAccountThemeContext !== undefined) {
-                return <KcAccountThemeApp kcContext={kcAccountThemeContext} />;
             }
 
             return <App />;
