0f630e7e072d4616e711995c577ab5592c5964e8
Joseph Garrone <joseph.garrone.gj@gmail.com>
2023-11-07 19:10:06 +0100

    Enable the favicon to be a png or an ico

--- web/src/ui/theme.tsx
+++ web/src/ui/theme.tsx
@@ -70,76 +70,102 @@ export const { useTheme, ThemeProvider } = createThemeProvider({
 
 export async function loadThemedFavicon() {
     evtIsDarkModeEnabled.attach(async isDarkModeEnabled => {
-        const svgUrl = resolveAssetVariantUrl({
+        const faviconUrl = resolveAssetVariantUrl({
             isDarkModeEnabled,
             "assetVariantUrl": env.FAVICON
         });
 
-        const rawSvgString = await fetch(svgUrl)
-            .then(response => response.text())
-            .catch(() => undefined);
-
-        if (rawSvgString === undefined) {
-            console.warn(`Failed to fetch favicon ${svgUrl}`);
-            return;
-        }
-
-        const svgRoot = (() => {
-            let svgElement: SVGSVGElement | null;
-
-            try {
-                const parser = new DOMParser();
-                const doc = parser.parseFromString(rawSvgString, "image/svg+xml");
-                svgElement = doc.querySelector("svg");
-            } catch (error) {
-                console.error(`Failed to parse ${svgUrl}, ${String(error)}`);
-                return undefined;
-            }
-
-            if (svgElement === null) {
-                console.error(`${svgUrl} is empty`);
-                return undefined;
-            }
-
-            return svgElement;
-        })();
-
-        if (svgRoot === undefined) {
-            return;
-        }
-
-        const colorUsecases = createDefaultColorUseCases({
-            palette,
-            isDarkModeEnabled
-        });
-
-        (function updateFillColor(element: Element) {
-            for (const [className, color] of [
-                ["focus-color-fill", colorUsecases.typography.textFocus],
-                ["text-color-fill", colorUsecases.typography.textPrimary],
-                ["surface-color-fill", colorUsecases.surfaces.surface1],
-                ["background-color-fill", colorUsecases.surfaces.background]
-            ] as const) {
-                if (element.getAttribute("class")?.includes(className)) {
-                    element.setAttribute("fill", color);
-                }
-            }
-
-            // Recursively update child elements
-            for (const child of Array.from(element.children)) {
-                updateFillColor(child);
-            }
-        })(svgRoot);
-
-        // Convert updated SVG DOM object back to string
-        const serializer = new XMLSerializer();
-        const updatedRawSvg = serializer.serializeToString(svgRoot);
-
-        const url = "data:image/svg+xml," + encodeURIComponent(updatedRawSvg);
-
         const link = document.querySelector("link[rel*='icon']");
         assert(link !== null, "Expect a favicon already present in the head");
-        Object.assign(link, { "type": "image/svg+xml", "href": url });
+
+        const linkProps = await (async (): Promise<
+            { type: string; href: string } | undefined
+        > => {
+            if (!faviconUrl.endsWith(".svg")) {
+                const extension = faviconUrl.split(".").pop();
+
+                assert(
+                    extension !== undefined && ["png", "ico"].includes(extension),
+                    "Expect a png or ico favicon"
+                );
+
+                return {
+                    "type": `image/${extension}`,
+                    "href": faviconUrl
+                };
+            }
+
+            const rawSvgString = await fetch(faviconUrl)
+                .then(response => response.text())
+                .catch(() => undefined);
+
+            if (rawSvgString === undefined) {
+                console.warn(`Failed to fetch favicon ${faviconUrl}`);
+                return undefined;
+            }
+
+            const svgRoot = (() => {
+                let svgElement: SVGSVGElement | null;
+
+                try {
+                    const parser = new DOMParser();
+                    const doc = parser.parseFromString(rawSvgString, "image/svg+xml");
+                    svgElement = doc.querySelector("svg");
+                } catch (error) {
+                    console.error(`Failed to parse ${faviconUrl}, ${String(error)}`);
+                    return undefined;
+                }
+
+                if (svgElement === null) {
+                    console.error(`${faviconUrl} is empty`);
+                    return undefined;
+                }
+
+                return svgElement;
+            })();
+
+            if (svgRoot === undefined) {
+                return undefined;
+            }
+
+            const colorUsecases = createDefaultColorUseCases({
+                palette,
+                isDarkModeEnabled
+            });
+
+            (function updateFillColor(element: Element) {
+                for (const [className, color] of [
+                    ["focus-color-fill", colorUsecases.typography.textFocus],
+                    ["text-color-fill", colorUsecases.typography.textPrimary],
+                    ["surface-color-fill", colorUsecases.surfaces.surface1],
+                    ["background-color-fill", colorUsecases.surfaces.background]
+                ] as const) {
+                    if (element.getAttribute("class")?.includes(className)) {
+                        element.setAttribute("fill", color);
+                    }
+                }
+
+                // Recursively update child elements
+                for (const child of Array.from(element.children)) {
+                    updateFillColor(child);
+                }
+            })(svgRoot);
+
+            // Convert updated SVG DOM object back to string
+            const serializer = new XMLSerializer();
+            const updatedRawSvg = serializer.serializeToString(svgRoot);
+
+            return {
+                "type": "image/svg+xml",
+                "href": "data:image/svg+xml," + encodeURIComponent(updatedRawSvg)
+            };
+        })();
+
+        if (linkProps === undefined) {
+            return;
+        }
+
+        Object.assign(link, linkProps);
 
         document.getElementsByTagName("head")[0].appendChild(link);
     });
