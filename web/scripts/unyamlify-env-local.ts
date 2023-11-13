a98a96fc8c710b7882988f38d7f833ad32101b81
Joseph Garrone <joseph.garrone.gj@gmail.com>
2023-11-13 13:24:22 +0100

    Generate message to make it clear that .env.local is generated

--- web/scripts/unyamlify-env-local.ts
+++ web/scripts/unyamlify-env-local.ts
@@ -4,17 +4,24 @@ import * as fs from "fs";
 
 const projectRootPath = pathJoin(__dirname, "..");
 
+const envLocalYamlBasename = ".env.local.yaml";
+
 const parsedEnvLocalYaml: { onyxia: { web: { env: Record<string, string> } } } =
     yaml.parse(
-        fs.readFileSync(pathJoin(projectRootPath, ".env.local.yaml")).toString("utf8")
+        fs.readFileSync(pathJoin(projectRootPath, envLocalYamlBasename)).toString("utf8")
     );
 
 fs.writeFileSync(
     pathJoin(projectRootPath, ".env.local"),
     Buffer.from(
-        Object.entries(parsedEnvLocalYaml.onyxia.web.env)
-            .map(([key, value]) => `REACT_APP_${key}=${value.replace(/\n/g, "")}`)
-            .join("\n"),
+        [
+            `# This file was generated automatically from ${envLocalYamlBasename}`,
+            `# Do not edit it manually!`,
+            "",
+            ...Object.entries(parsedEnvLocalYaml.onyxia.web.env).map(
+                ([key, value]) => `REACT_APP_${key}=${value.replace(/\n/g, "")}`
+            )
+        ].join("\n"),
         "utf8"
     )
 );
