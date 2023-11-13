415e5b0639c42ea43c4f8c77c4d1ee8ba89529dd
Joseph Garrone <joseph.garrone.gj@gmail.com>
2023-11-13 15:40:55 +0100

    Automatically create the .env.local.yaml file if not exist

--- web/scripts/unyamlify-env-local.ts
+++ web/scripts/unyamlify-env-local.ts
@@ -1,21 +1,58 @@
 import * as yaml from "yaml";
-import { join as pathJoin } from "path";
+import { join as pathJoin, basename as pathBasename } from "path";
 import * as fs from "fs";
 
 const projectRootPath = pathJoin(__dirname, "..");
 
-const envLocalYamlBasename = ".env.local.yaml";
+const envLocalYamlFilePath = pathJoin(projectRootPath, ".env.local.yaml");
+
+if (!fs.existsSync(envLocalYamlFilePath)) {
+    fs.writeFileSync(
+        envLocalYamlFilePath,
+        Buffer.from(
+            [
+                `# You can edit this file to test the web app in different configuration locally`,
+                `# After editing this file you must re-run: yarn start`,
+                ``,
+                `onyxia:`,
+                `  web:`,
+                `    env:`,
+                `      ONYXIA_API_URL: https://datalab.sspcloud.fr/api`,
+                `      HEADER_TEXT_BOLD: SSP Cloud`,
+                `      HEADER_TEXT_FOCUS: Datalab`,
+                `      TERMS_OF_SERVICES: |`,
+                `        { `,
+                `          "en": "https://www.sspcloud.fr/tos_en.md",`,
+                `          "fr": "https://www.sspcloud.fr/tos_fr.md"`,
+                `        }`,
+                `      HEADER_LINKS: |`,
+                `        [`,
+                `          {`,
+                `            "label": {`,
+                `              "en": "Tutorials",`,
+                `              "fr": "Tutoriels"`,
+                `            },`,
+                `            "icon": "https://www.sspcloud.fr/trainings.svg",`,
+                `            "url": "https://www.sspcloud.fr/formation"`,
+                `          }`,
+                `        ]`,
+                ``
+            ].join("\n"),
+            "utf8"
+        )
+    );
+}
 
 const parsedEnvLocalYaml: { onyxia: { web: { env: Record<string, string> } } } =
-    yaml.parse(
-        fs.readFileSync(pathJoin(projectRootPath, envLocalYamlBasename)).toString("utf8")
-    );
+    yaml.parse(fs.readFileSync(envLocalYamlFilePath).toString("utf8"));
 
 fs.writeFileSync(
     pathJoin(projectRootPath, ".env.local"),
     Buffer.from(
         [
-            `# This file was generated automatically from ${envLocalYamlBasename}`,
+            `# This file was generated automatically from ${pathBasename(
+                envLocalYamlFilePath
+            )}`,
             `# Do not edit it manually!`,
             "",
             ...Object.entries(parsedEnvLocalYaml.onyxia.web.env).map(
