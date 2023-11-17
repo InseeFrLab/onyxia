44eeb4b2b6e5955d9a0c53d05193ae910fe12bf4
Joseph Garrone <joseph.garrone.gj@gmail.com>
2023-11-17 04:35:08 +0100

    Properly overwrite palette

--- web/src/ui/theme/palette.ts
+++ web/src/ui/theme/palette.ts
@@ -2,15 +2,18 @@ import { defaultPalette } from "onyxia-ui";
 import { env } from "env-parsed";
 import { mergeDeep } from "ui/tools/mergeDeep";
 
-export const palette = {
-    ...mergeDeep(defaultPalette, env.PALETTE_OVERRIDE),
-    "limeGreen": {
-        "main": "#BAFF29",
-        "light": "#E2FFA6"
+export const palette = mergeDeep(
+    {
+        ...defaultPalette,
+        "limeGreen": {
+            "main": "#BAFF29",
+            "light": "#E2FFA6"
+        },
+        "agentConnectBlue": {
+            "main": "#0579EE",
+            "light": "#2E94FA",
+            "lighter": "#E5EDF5"
+        }
     },
-    "agentConnectBlue": {
-        "main": "#0579EE",
-        "light": "#2E94FA",
-        "lighter": "#E5EDF5"
-    }
-};
+    env.PALETTE_OVERRIDE
+);
