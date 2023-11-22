935f5a94650a8329697113b6ed64c853b6aa4f55
Trygve Falch <trygve.falch@gmail.com>
2023-11-22 11:18:54 +0100

    Added isSensitiveInformation to the vaulttab.

--- web/src/ui/pages/account/tabs/AccountVaultTab.tsx
+++ web/src/ui/pages/account/tabs/AccountVaultTab.tsx
@@ -102,6 +102,7 @@ export const AccountVaultTab = memo((props: Props) => {
                         "text": uiState[key]
                     })}
                     onRequestCopy={onFieldRequestCopyFactory(uiState[key])}
+                    isSensitiveInformation={key === "vaultToken"}
                 />
             ))}
             <Divider className={classes.divider} variant="middle" />
