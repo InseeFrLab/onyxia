8f1b8c0d1f0c29004c559b80044d647655c91bc7
Joseph Garrone <joseph.garrone.gj@gmail.com>
2023-11-23 06:36:43 +0100

    DuckDB: Update Bundler config to deliver the workers as static assets

--- web/config-overrides.js
+++ web/config-overrides.js
@@ -7,44 +7,58 @@ This file is picked up by react-app-rewired that we use in place or react-script
 // This is an webpack extension to detect circular import (example:  A imports B that imports A)
 const CircularDependencyPlugin = require("circular-dependency-plugin");
 const { DefinePlugin, ProvidePlugin } = require("webpack");
+const { assert } = require("tsafe/assert");
 
 module.exports = function override(config) {
-    if (!config.resolve.fallback) {
-        config.resolve.fallback = {};
+
+    Object.assign(config.resolve.fallback ??= {}, {
+        "crypto": require.resolve("crypto-browserify"),
+        "stream": require.resolve("stream-browserify"),
+        "http": require.resolve("stream-http"),
+        "https": require.resolve("https-browserify"),
+        "timers": require.resolve("timers-browserify"),
+        "path": require.resolve("path-browserify")
+    });
+
+    (config.plugins ??= []).push(...[
+        new CircularDependencyPlugin({
+            // exclude detection of files based on a RegExp
+            "exclude": /node_modules/,
+            // add errors to webpack instead of warnings
+            "failOnError": true,
+            // allow import cycles that include an asynchronous import,
+            // e.g. via import(/* webpackMode: "weak" */ './file.js')
+            "allowAsyncCycles": false,
+            // set the current working directory for displaying module paths
+            "cwd": process.cwd(),
+        }),
+        new ProvidePlugin({
+            "process": "process",
+        }),
+        new DefinePlugin({
+            "process.env.WEB_VERSION": JSON.stringify(process.env.npm_package_version)
+        })
+    ]);
+
+    {
+
+        const sourceMapLoaderRule = config.module.rules.find(({ loader }) => loader.includes("source-map-loader"));
+
+        assert(sourceMapLoaderRule !== undefined);
+
+        sourceMapLoaderRule.test = /((?<!\.worker)\.(js|mjs))|(\.(jsx|ts|tsx|css))$/;
+
     }
 
-    config.resolve.fallback["crypto"] = require.resolve("crypto-browserify");
-    config.resolve.fallback["stream"] = require.resolve("stream-browserify");
-    config.resolve.fallback["http"] = require.resolve("stream-http");
-    config.resolve.fallback["https"] = require.resolve("https-browserify");
-    config.resolve.fallback["timers"] = require.resolve("timers-browserify");
-    config.resolve.fallback["path"] = require.resolve("path-browserify");
-
-    if (!config.plugins) {
-        config.plugins = [];
-    }
-
-    config.plugins.push(
-        ...[
-            new CircularDependencyPlugin({
-                // exclude detection of files based on a RegExp
-                "exclude": /node_modules/,
-                // add errors to webpack instead of warnings
-                "failOnError": true,
-                // allow import cycles that include an asynchronous import,
-                // e.g. via import(/* webpackMode: "weak" */ './file.js')
-                "allowAsyncCycles": false,
-                // set the current working directory for displaying module paths
-                "cwd": process.cwd(),
-            }),
-            new ProvidePlugin({
-                "process": "process",
-            }),
-            new DefinePlugin({
-                "process.env.WEB_VERSION": JSON.stringify(process.env.npm_package_version)
-            }),
-        ],
-    );
+    config.module.rules.push(...[
+        {
+            "test": /\.worker\.js$/i,
+            "type": "asset/resource",
+            "generator": {
+                "filename": "static/js/[name].[hash:8][ext]",
+            }
+        }
+    ]);
 
     return config;
 };
