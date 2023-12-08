66684c399af9877f730d0a8a7d718ce87f982e12
Joseph Garrone <joseph.garrone.gj@gmail.com>
2023-12-08 16:58:16 +0100

    Version of Onyxia with debug logging for oidc edge case issue

--- web/src/core/adapters/oidc/default.ts
+++ web/src/core/adapters/oidc/default.ts
@@ -11,6 +11,7 @@ export async function createOidc(params: {
     return createOidcSpa({
         issuerUri,
         clientId,
-        transformUrlBeforeRedirect
+        transformUrlBeforeRedirect,
+        "log": console.log
     });
 }
