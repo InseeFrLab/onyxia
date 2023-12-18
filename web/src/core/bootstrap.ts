1ece9163ef479d05120501a9bc61e22dd25c22e8
Joseph Garrone <joseph.garrone.gj@gmail.com>
2023-12-18 10:28:10 +0100

    dynamically swap s3Client adapter depending on context

--- web/src/core/bootstrap.ts
+++ web/src/core/bootstrap.ts
@@ -12,8 +12,12 @@ import type { SecretsManager } from "core/ports/SecretsManager";
 import type { Oidc } from "core/ports/Oidc";
 import type { S3Client } from "core/ports/S3Client";
 import type { Language } from "core/ports/OnyxiaApi/Language";
+import type { ParamsOfCreateS3Client } from "core/adapters/s3Client/default";
 import { createDuckDbSqlOlap } from "core/adapters/sqlOlap/default";
 import { pluginSystemInitCore } from "pluginSystem";
+import { assert } from "tsafe/assert";
+import { id } from "tsafe/id";
+import { onlyIfChanged } from "evt/operators";
 
 type ParamsOfBootstrapCore = {
     /** Empty string for using mock */
@@ -40,12 +44,14 @@ export async function bootstrapCore(
 ): Promise<{ core: Core }> {
     const { apiUrl, transformUrlBeforeRedirectToLogin } = params;
 
+    const isSandboxEnvironment = apiUrl === "";
+
     let isCoreCreated = false;
 
     let oidc: Oidc | undefined = undefined;
 
     const onyxiaApi = await (async () => {
-        if (apiUrl === "") {
+        if (isSandboxEnvironment) {
             const { onyxiaApi } = await import("core/adapters/onyxiaApi/mock");
 
             return onyxiaApi;
@@ -65,13 +71,13 @@ export async function bootstrapCore(
                 }
                 return oidc.getTokens().accessToken;
             },
-            "getRegionId": () => {
+            "getCurrentRegionId": () => {
                 if (!isCoreCreated) {
                     return undefined;
                 }
 
                 try {
-                    return usecases.deploymentRegion.selectors.selectedDeploymentRegion(
+                    return usecases.deploymentRegionSelection.selectors.currentDeploymentRegion(
                         getState()
                     ).id;
                 } catch (error) {
@@ -81,19 +87,27 @@ export async function bootstrapCore(
                     throw error;
                 }
             },
-            "getProject": () => {
+            "getCurrentProjectIdAndGroup": () => {
                 if (!isCoreCreated) {
                     return undefined;
                 }
 
+                let id: string;
+                let group: string | undefined;
+
                 try {
-                    return usecases.projectConfigs.selectors.selectedProject(getState());
+                    const project = usecases.projectSelection.selectors.currentProject(
+                        getState()
+                    );
+                    id = project.id;
+                    group = project.group;
                 } catch (error) {
                     if (error instanceof AccessError) {
                         return undefined;
                     }
                     throw error;
                 }
+                return { id, group };
             }
         });
 
@@ -124,18 +138,16 @@ export async function bootstrapCore(
         "paramsOfBootstrapCore": params,
         oidc,
         onyxiaApi,
-        /** prettier-ignore */
         "secretsManager": createObjectThatThrowsIfAccessed<SecretsManager>({
             "debugMessage": "secretsManager is not yet initialized"
         }),
-        /** prettier-ignore */
         "s3Client": createObjectThatThrowsIfAccessed<S3Client>({
             "debugMessage": "s3 client is not yet initialized"
         }),
         "sqlOlap": createDuckDbSqlOlap()
     };
 
-    const { core, dispatch, getState } = createCore({
+    const { core, dispatch, getState, evtAction } = createCore({
         context,
         usecases
     });
@@ -144,69 +156,206 @@ export async function bootstrapCore(
 
     await dispatch(usecases.userAuthentication.protectedThunks.initialize());
 
-    await dispatch(usecases.deploymentRegion.protectedThunks.initialize());
+    await dispatch(usecases.deploymentRegionSelection.protectedThunks.initialize());
 
     if (oidc.isUserLoggedIn) {
-        /* prettier-ignore */
-        const { s3: s3Params, vault: vaultParams } = usecases.deploymentRegion.selectors.selectedDeploymentRegion(getState());
-
-        /* prettier-ignore */
-        const nonMockOidc = oidcParams === undefined ? undefined : oidc;
-
-        /* prettier-ignore */
-        const { createOidcOrFallback } = await import("core/adapters/oidc/utils/createOidcOrFallback");
-
-        context.s3Client = await (async () => {
-            if (s3Params === undefined) {
-                const { s3client } = await import("core/adapters/s3Client/mock");
-
-                return s3client;
-            }
-
-            /** prettier-ignore */
-            const { createS3Client, getCreateS3ClientParams } = await import(
-                "core/adapters/s3Client/default"
-            );
-
-            return createS3Client({
-                ...getCreateS3ClientParams({ s3Params }),
-                "createAwsBucket": onyxiaApi.createAwsBucket,
-                "oidc": await createOidcOrFallback({
-                    "oidcAdapterImplementationToUseIfNotFallingBack": "default",
-                    "oidcParams": s3Params.oidcParams,
-                    "fallbackOidc": nonMockOidc
-                })
-            });
-        })();
-
         context.secretsManager = await (async () => {
-            if (vaultParams === undefined) {
+            /* prettier-ignore */
+            const deploymentRegion = usecases.deploymentRegionSelection.selectors.currentDeploymentRegion(getState());
+
+            if (deploymentRegion.vault === undefined) {
                 /* prettier-ignore */
                 const { createSecretManager } = await import("core/adapters/secretManager/mock");
 
                 return createSecretManager();
             }
 
-            const { createSecretManager } = await import(
-                "core/adapters/secretManager/default"
+            const [{ createSecretManager }, { createOidcOrFallback }] = await Promise.all(
+                [
+                    import("core/adapters/secretManager/default"),
+                    import("core/adapters/oidc/utils/createOidcOrFallback")
+                ]
             );
 
             return createSecretManager({
-                "kvEngine": vaultParams.kvEngine,
-                "role": vaultParams.role,
-                "url": vaultParams.url,
-                "authPath": vaultParams.authPath,
+                "kvEngine": deploymentRegion.vault.kvEngine,
+                "role": deploymentRegion.vault.role,
+                "url": deploymentRegion.vault.url,
+                "authPath": deploymentRegion.vault.authPath,
                 "oidc": await createOidcOrFallback({
                     "oidcAdapterImplementationToUseIfNotFallingBack": "default",
-                    "oidcParams": vaultParams.oidcParams,
-                    "fallbackOidc": nonMockOidc
+                    "oidcParams": deploymentRegion.vault.oidcParams,
+                    "fallbackOidc": oidcParams === undefined ? undefined : oidc
                 })
             });
         })();
 
         await dispatch(usecases.userConfigs.protectedThunks.initialize());
 
+        await dispatch(usecases.projectSelection.protectedThunks.initialize());
+
         await dispatch(usecases.projectConfigs.protectedThunks.initialize());
+
+        init_s3_client: {
+            if (isSandboxEnvironment) {
+                const { s3client } = await import("core/adapters/s3Client/mock");
+
+                context.s3Client = s3client;
+
+                break init_s3_client;
+            }
+
+            const [{ createS3Client }, { createOidcOrFallback }] = await Promise.all([
+                import("core/adapters/s3Client/default"),
+                import("core/adapters/oidc/utils/createOidcOrFallback")
+            ]);
+
+            const deploymentRegion =
+                usecases.deploymentRegionSelection.selectors.currentDeploymentRegion(
+                    getState()
+                );
+
+            const oidcForS3 =
+                deploymentRegion.s3Params.serverIntegratedWithOidc === undefined
+                    ? undefined
+                    : await createOidcOrFallback({
+                          "oidcAdapterImplementationToUseIfNotFallingBack": "default",
+                          "oidcParams":
+                              deploymentRegion.s3Params.serverIntegratedWithOidc
+                                  .oidcParams,
+                          "fallbackOidc": oidcParams === undefined ? undefined : oidc
+                      });
+
+            evtAction
+                .pipe(
+                    data =>
+                        data.usecaseName === "projectConfigs" &&
+                        (data.actionName === "projectChanged" ||
+                            (data.actionName === "updated" &&
+                                data.payload.key === "customS3Configs"))
+                )
+                .toStateful()
+                .pipe(() => {
+                    const { customS3Configs } =
+                        usecases.projectConfigs.selectors.selectedProjectConfigs(
+                            getState()
+                        );
+
+                    const project = usecases.projectSelection.selectors.currentProject(
+                        getState()
+                    );
+
+                    return [
+                        {
+                            customS3Configs,
+                            "projectBucketName": project.bucket
+                        }
+                    ];
+                })
+                .pipe(onlyIfChanged())
+                .attach(({ customS3Configs, projectBucketName }) => {
+                    init_with_region_params: {
+                        if (customS3Configs.indexForXOnyxia === undefined) {
+                            break init_with_region_params;
+                        }
+
+                        const {
+                            url,
+                            region,
+                            accessKeyId,
+                            secretAccessKey,
+                            sessionToken
+                        } =
+                            customS3Configs.availableConfigs[
+                                customS3Configs.indexForXOnyxia
+                            ];
+
+                        context.s3Client = createS3Client({
+                            "authWith": "provided S3 account credentials",
+                            url,
+                            region,
+                            accessKeyId,
+                            secretAccessKey,
+                            sessionToken
+                        });
+
+                        return;
+                    }
+
+                    const deploymentRegion =
+                        usecases.deploymentRegionSelection.selectors.currentDeploymentRegion(
+                            getState()
+                        );
+
+                    if (
+                        deploymentRegion.s3Params.serverIntegratedWithOidc === undefined
+                    ) {
+                        return;
+                    }
+
+                    assert(oidcForS3 !== undefined);
+
+                    const common: ParamsOfCreateS3Client.AuthWithVolatileAccount.Common =
+                        {
+                            "authWith":
+                                "volatile S3 Account dynamically created with OIDC",
+                            "oidc": oidcForS3,
+                            "requestedTokenValidityDurationSeconds":
+                                deploymentRegion.s3Params.serverIntegratedWithOidc
+                                    .requestedTokenValidityDurationSeconds,
+                            "nameOfBucketToCreateIfNotExist": deploymentRegion.s3Params
+                                .serverIntegratedWithOidc.doSupportDynamicBucketCreation
+                                ? projectBucketName
+                                : undefined,
+                            "region":
+                                deploymentRegion.s3Params.serverIntegratedWithOidc.region,
+                            "roleARN":
+                                deploymentRegion.s3Params.serverIntegratedWithOidc
+                                    .roleARN,
+                            "roleSessionName":
+                                deploymentRegion.s3Params.serverIntegratedWithOidc
+                                    .roleSessionName
+                        };
+
+                    const paramsOfCreateS3Client = deploymentRegion.s3Params
+                        .serverIntegratedWithOidc.isOnPremise
+                        ? id<ParamsOfCreateS3Client.AuthWithVolatileAccount.OnPremise>({
+                              ...common,
+                              "isOnPremise": true,
+                              "url": deploymentRegion.s3Params.serverIntegratedWithOidc
+                                  .url
+                          })
+                        : (() => {
+                              switch (
+                                  deploymentRegion.s3Params.serverIntegratedWithOidc
+                                      .cloudProvider
+                              ) {
+                                  case "Amazon web services":
+                                      return id<ParamsOfCreateS3Client.AuthWithVolatileAccount.CloudProvider.AmazonWebServices>(
+                                          {
+                                              ...common,
+                                              "isOnPremise": false,
+                                              "cloudProvider": "Amazon web services",
+                                              "region":
+                                                  deploymentRegion.s3Params
+                                                      .serverIntegratedWithOidc.region,
+                                              "roleARN":
+                                                  deploymentRegion.s3Params
+                                                      .serverIntegratedWithOidc.roleARN,
+                                              "roleSessionName":
+                                                  deploymentRegion.s3Params
+                                                      .serverIntegratedWithOidc
+                                                      .roleSessionName,
+                                              "createAwsBucket":
+                                                  context.onyxiaApi.createAwsBucket
+                                          }
+                                      );
+                              }
+                          })();
+
+                    context.s3Client = createS3Client(paramsOfCreateS3Client);
+                });
+        }
 
         await dispatch(usecases.restorableConfigManager.protectedThunks.initialize());
 
