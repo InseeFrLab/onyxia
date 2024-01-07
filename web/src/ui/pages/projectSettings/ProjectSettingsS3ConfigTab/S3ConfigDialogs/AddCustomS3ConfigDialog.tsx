c2afcbc35f4d4f00cba7a92d232a0111b25e3a16
Joseph Garrone <joseph.garrone.gj@gmail.com>
2024-01-07 05:14:15 +0100

    Make the s3 creation dialog scrollable

--- web/src/ui/pages/projectSettings/ProjectSettingsS3ConfigTab/S3ConfigDialogs/AddCustomS3ConfigDialog.tsx
+++ web/src/ui/pages/projectSettings/ProjectSettingsS3ConfigTab/S3ConfigDialogs/AddCustomS3ConfigDialog.tsx
@@ -12,6 +12,7 @@ import RadioGroup from "@mui/material/RadioGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import FormControl from "@mui/material/FormControl";
 import FormLabel from "@mui/material/FormLabel";
+import { useWindowInnerSize } from "powerhooks/useWindowInnerSize";
 import { tss } from "tss";
 
 export type Props = {
@@ -84,14 +85,24 @@ export const AddCustomS3ConfigDialog = memo((props: Props) => {
         setState(undefined);
     });
 
+    const { windowInnerHeight } = useWindowInnerSize();
+
+    const { classes } = useStyles({
+        windowInnerHeight
+    });
+
     return (
         <Dialog
             title="New custom S3 configuration"
+            className={classes.root}
             body={
                 state === undefined ? (
                     <></>
                 ) : (
-                    <Body evtNewCustomConfig={state.evtNewCustomConfig} />
+                    <Body
+                        className={classes.body}
+                        evtNewCustomConfig={state.evtNewCustomConfig}
+                    />
                 )
             }
             buttons={
@@ -114,185 +125,206 @@ AddCustomS3ConfigDialog.displayName = symToStr({
     AddCustomS3ConfigDialog
 });
 
-const Body = memo((props: { evtNewCustomConfig: StatefulEvt<NewCustomConfig> }) => {
-    const { evtNewCustomConfig } = props;
+const useStyles = tss
+    .withName({ AddCustomS3ConfigDialog })
+    .withParams<{ windowInnerHeight: number }>()
+    .create(({ theme, windowInnerHeight }) => ({
+        "root": {
+            "display": "flex",
+            "flexDirection": "column",
+            "maxHeight": windowInnerHeight - theme.spacing(4)
+        },
+        "body": {
+            "flex": 1,
+            "overflow": "auto"
+        }
+    }));
 
-    useRerenderOnStateChange(evtNewCustomConfig);
+const Body = memo(
+    (props: { className?: string; evtNewCustomConfig: StatefulEvt<NewCustomConfig> }) => {
+        const { className, evtNewCustomConfig } = props;
 
-    const { classes } = useStyles();
+        useRerenderOnStateChange(evtNewCustomConfig);
 
-    return (
-        <div className={classes.root}>
-            <TextField
-                className={classes.textField}
-                label="URL"
-                helperText="The URL of the S3 endpoint"
-                defaultValue={evtNewCustomConfig.state.url}
-                doOnlyValidateInputAfterFistFocusLost
-                getIsValidValue={url => {
-                    try {
-                        new URL(url);
-                    } catch (error) {
+        const { classes, cx } = useBodyStyles();
+
+        return (
+            <div className={cx(classes.root, className)}>
+                <TextField
+                    className={classes.textField}
+                    label="URL"
+                    helperText="The URL of the S3 endpoint"
+                    defaultValue={evtNewCustomConfig.state.url}
+                    doOnlyValidateInputAfterFistFocusLost
+                    getIsValidValue={url => {
+                        try {
+                            new URL(url);
+                        } catch (error) {
+                            return {
+                                "isValidValue": false,
+                                "message": "Invalid URL"
+                            };
+                        }
+
                         return {
-                            "isValidValue": false,
-                            "message": "Invalid URL"
+                            "isValidValue": true
                         };
+                    }}
+                    onValueBeingTypedChange={({ value }) =>
+                        (evtNewCustomConfig.state = {
+                            ...evtNewCustomConfig.state,
+                            "url": value
+                        })
                     }
-
-                    return {
-                        "isValidValue": true
-                    };
-                }}
-                onValueBeingTypedChange={({ value }) =>
-                    (evtNewCustomConfig.state = {
-                        ...evtNewCustomConfig.state,
-                        "url": value
-                    })
-                }
-            />
-            <TextField
-                className={classes.textField}
-                label="Region"
-                helperText="The region of the S3 endpoint"
-                defaultValue={evtNewCustomConfig.state.region}
-                onValueBeingTypedChange={({ value }) =>
-                    (evtNewCustomConfig.state = {
-                        ...evtNewCustomConfig.state,
-                        "region": value
-                    })
-                }
-            />
-            <TextField
-                className={classes.textField}
-                label="Working directory path"
-                helperText="<bucket>/<object prefix> example: my-bucket/my-directory/"
-                defaultValue={evtNewCustomConfig.state.workingDirectoryPath}
-                onValueBeingTypedChange={({ value }) => {
-                    evtNewCustomConfig.state = {
-                        ...evtNewCustomConfig.state,
-                        "workingDirectoryPath": value
-                    };
-                }}
-            />
-            <TextField
-                className={classes.textField}
-                label="Account friendly name"
-                helperText="This is just to help you identify the account"
-                defaultValue={evtNewCustomConfig.state.accountFriendlyName}
-                onValueBeingTypedChange={({ value }) =>
-                    (evtNewCustomConfig.state = {
-                        ...evtNewCustomConfig.state,
-                        "accountFriendlyName": value
-                    })
-                }
-            />
-            <TextField
-                className={classes.textField}
-                label="Access key ID"
-                defaultValue={evtNewCustomConfig.state.accessKeyId}
-                onValueBeingTypedChange={({ value }) =>
-                    (evtNewCustomConfig.state = {
-                        ...evtNewCustomConfig.state,
-                        "accessKeyId": value
-                    })
-                }
-            />
-            <TextField
-                className={classes.textField}
-                label="Secret access key"
-                defaultValue={evtNewCustomConfig.state.secretAccessKey}
-                onValueBeingTypedChange={({ value }) =>
-                    (evtNewCustomConfig.state = {
-                        ...evtNewCustomConfig.state,
-                        "secretAccessKey": value
-                    })
-                }
-            />
-            <TextField
-                className={classes.textField}
-                label="Session token"
-                helperText="Optional"
-                defaultValue={evtNewCustomConfig.state.sessionToken ?? ""}
-                onValueBeingTypedChange={({ value }) =>
-                    (evtNewCustomConfig.state = {
-                        ...evtNewCustomConfig.state,
-                        "sessionToken": value || undefined
-                    })
-                }
-            />
-            <FormControl>
-                <FormLabel id="path-style">Url style</FormLabel>
-                <RadioGroup
-                    aria-labelledby="path-style"
-                    value={
-                        evtNewCustomConfig.state.pathStyleAccess ? "path" : "subdomain"
+                />
+                <TextField
+                    className={classes.textField}
+                    label="Region"
+                    helperText="The region of the S3 endpoint"
+                    defaultValue={evtNewCustomConfig.state.region}
+                    onValueBeingTypedChange={({ value }) =>
+                        (evtNewCustomConfig.state = {
+                            ...evtNewCustomConfig.state,
+                            "region": value
+                        })
                     }
-                    onChange={(_, value) => {
+                />
+                <TextField
+                    className={classes.textField}
+                    label="Working directory path"
+                    helperText="<bucket>/<object prefix> example: my-bucket/my-directory/"
+                    defaultValue={evtNewCustomConfig.state.workingDirectoryPath}
+                    onValueBeingTypedChange={({ value }) => {
                         evtNewCustomConfig.state = {
                             ...evtNewCustomConfig.state,
-                            "pathStyleAccess": value === "path"
+                            "workingDirectoryPath": value
                         };
                     }}
-                >
-                    <FormControlLabel
-                        value="path"
-                        control={<Radio />}
-                        label={`Path style (${evtNewCustomConfig.state.url}/<bucket>/<object name>)`}
-                    />
-                    <FormControlLabel
-                        value="subdomain"
-                        control={<Radio />}
-                        label={`Subdomain style (<bucket>.${evtNewCustomConfig.state.url}/<object name>)`}
-                    />
-                </RadioGroup>
-            </FormControl>
-            <FormControl>
-                <FormLabel id="use-for-injection">Use in services</FormLabel>
-                <RadioGroup
-                    aria-labelledby="use-for-injection"
-                    value={evtNewCustomConfig.state.isUsedForXOnyxia ? "yes" : "no"}
-                    onChange={(_, value) => {
-                        evtNewCustomConfig.state = {
+                />
+                <TextField
+                    className={classes.textField}
+                    label="Account friendly name"
+                    helperText="This is just to help you identify the account"
+                    defaultValue={evtNewCustomConfig.state.accountFriendlyName}
+                    onValueBeingTypedChange={({ value }) =>
+                        (evtNewCustomConfig.state = {
                             ...evtNewCustomConfig.state,
-                            "isUsedForXOnyxia": value === "yes"
-                        };
-                    }}
-                >
-                    <FormControlLabel
-                        value="yes"
-                        control={<Radio />}
-                        label={`Yes, use this configuration as default for services`}
-                    />
-                    <FormControlLabel value="no" control={<Radio />} label="No" />
-                </RadioGroup>
-            </FormControl>
-            <FormControl>
-                <FormLabel id="use-for-explorer">Use for the explorer</FormLabel>
-                <RadioGroup
-                    aria-labelledby="use-for-explorer"
-                    value={evtNewCustomConfig.state.isUsedForExplorer ? "yes" : "no"}
-                    onChange={(_, value) => {
-                        evtNewCustomConfig.state = {
+                            "accountFriendlyName": value
+                        })
+                    }
+                />
+                <TextField
+                    className={classes.textField}
+                    label="Access key ID"
+                    defaultValue={evtNewCustomConfig.state.accessKeyId}
+                    onValueBeingTypedChange={({ value }) =>
+                        (evtNewCustomConfig.state = {
                             ...evtNewCustomConfig.state,
-                            "isUsedForExplorer": value === "yes"
-                        };
-                    }}
-                >
-                    <FormControlLabel value="yes" control={<Radio />} label="Yes" />
-                    <FormControlLabel value="no" control={<Radio />} label="No" />
-                </RadioGroup>
-            </FormControl>
-        </div>
-    );
-});
-
-const useStyles = tss.withName({ AddCustomS3ConfigDialog }).create(({ theme }) => ({
-    "root": {
-        "display": "flex",
-        "flexDirection": "column",
-        "marginTop": theme.spacing(4)
-    },
-    "textField": {
-        "marginBottom": theme.spacing(6)
+                            "accessKeyId": value
+                        })
+                    }
+                />
+                <TextField
+                    className={classes.textField}
+                    label="Secret access key"
+                    defaultValue={evtNewCustomConfig.state.secretAccessKey}
+                    onValueBeingTypedChange={({ value }) =>
+                        (evtNewCustomConfig.state = {
+                            ...evtNewCustomConfig.state,
+                            "secretAccessKey": value
+                        })
+                    }
+                />
+                <TextField
+                    className={classes.textField}
+                    label="Session token"
+                    helperText="Optional"
+                    defaultValue={evtNewCustomConfig.state.sessionToken ?? ""}
+                    onValueBeingTypedChange={({ value }) =>
+                        (evtNewCustomConfig.state = {
+                            ...evtNewCustomConfig.state,
+                            "sessionToken": value || undefined
+                        })
+                    }
+                />
+                <FormControl>
+                    <FormLabel id="path-style">Url style</FormLabel>
+                    <RadioGroup
+                        aria-labelledby="path-style"
+                        value={
+                            evtNewCustomConfig.state.pathStyleAccess
+                                ? "path"
+                                : "subdomain"
+                        }
+                        onChange={(_, value) => {
+                            evtNewCustomConfig.state = {
+                                ...evtNewCustomConfig.state,
+                                "pathStyleAccess": value === "path"
+                            };
+                        }}
+                    >
+                        <FormControlLabel
+                            value="path"
+                            control={<Radio />}
+                            label={`Path style (${evtNewCustomConfig.state.url}/<bucket>/<object name>)`}
+                        />
+                        <FormControlLabel
+                            value="subdomain"
+                            control={<Radio />}
+                            label={`Subdomain style (<bucket>.${evtNewCustomConfig.state.url}/<object name>)`}
+                        />
+                    </RadioGroup>
+                </FormControl>
+                <FormControl>
+                    <FormLabel id="use-for-injection">Use in services</FormLabel>
+                    <RadioGroup
+                        aria-labelledby="use-for-injection"
+                        value={evtNewCustomConfig.state.isUsedForXOnyxia ? "yes" : "no"}
+                        onChange={(_, value) => {
+                            evtNewCustomConfig.state = {
+                                ...evtNewCustomConfig.state,
+                                "isUsedForXOnyxia": value === "yes"
+                            };
+                        }}
+                    >
+                        <FormControlLabel
+                            value="yes"
+                            control={<Radio />}
+                            label={`Yes, use this configuration as default for services`}
+                        />
+                        <FormControlLabel value="no" control={<Radio />} label="No" />
+                    </RadioGroup>
+                </FormControl>
+                <FormControl>
+                    <FormLabel id="use-for-explorer">Use for the explorer</FormLabel>
+                    <RadioGroup
+                        aria-labelledby="use-for-explorer"
+                        value={evtNewCustomConfig.state.isUsedForExplorer ? "yes" : "no"}
+                        onChange={(_, value) => {
+                            evtNewCustomConfig.state = {
+                                ...evtNewCustomConfig.state,
+                                "isUsedForExplorer": value === "yes"
+                            };
+                        }}
+                    >
+                        <FormControlLabel value="yes" control={<Radio />} label="Yes" />
+                        <FormControlLabel value="no" control={<Radio />} label="No" />
+                    </RadioGroup>
+                </FormControl>
+            </div>
+        );
     }
-}));
+);
+
+const useBodyStyles = tss
+    .withName(`${symToStr({ AddCustomS3ConfigDialog })}${symToStr({ Body })}`)
+    .create(({ theme }) => ({
+        "root": {
+            "display": "flex",
+            "flexDirection": "column",
+            "marginTop": theme.spacing(4)
+        },
+        "textField": {
+            "marginBottom": theme.spacing(6)
+        }
+    }));
