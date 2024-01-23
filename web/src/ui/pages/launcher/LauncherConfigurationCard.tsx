32dff3f1d23edaf12badeb9596b300ea0009758c
Joseph Garrone <joseph.garrone.gj@gmail.com>
2024-01-23 18:51:42 +0100

    Fix error in register page

--- web/src/ui/pages/launcher/LauncherConfigurationCard.tsx
+++ web/src/ui/pages/launcher/LauncherConfigurationCard.tsx
@@ -449,7 +449,7 @@ const { TabContent } = (() => {
                                                     label={label}
                                                     defaultValue={formField.value.yamlStr}
                                                     helperText={helperText}
-                                                    helperTextError={hasError}
+                                                    isErrored={hasError}
                                                     disabled={formField.isReadonly}
                                                     selectAllTextOnFocus={false}
                                                     onValueBeingTypedChange={onYamlValueBeingChangeFactory(
@@ -532,7 +532,7 @@ const { TabContent } = (() => {
                                                     label={label}
                                                     defaultValue={formField.value}
                                                     helperText={helperText}
-                                                    helperTextError={hasError}
+                                                    isErrored={hasError}
                                                     disabled={formField.isReadonly}
                                                     onValueBeingTypedChange={onValueBeingChangeFactory(
                                                         JSON.stringify(formField.path)
