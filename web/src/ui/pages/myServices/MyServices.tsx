465132308b751d609cdfe0ebeac3024916173bba
Joseph Garrone <joseph.garrone.gj@gmail.com>
2024-03-27 19:59:54 +0100

    Fix eslint error

--- web/src/ui/pages/myServices/MyServices.tsx
+++ web/src/ui/pages/myServices/MyServices.tsx
@@ -69,7 +69,7 @@ export default function MyServices(props: Props) {
                 serviceManagement.update();
                 evtQuotasActionUpdate.post();
                 return;
-            case "trash":
+            case "trash": {
                 const dDoProceed = new Deferred<boolean>();
 
                 evtConfirmDeleteDialogOpen.post({
@@ -86,6 +86,7 @@ export default function MyServices(props: Props) {
                 );
 
                 return;
+            }
         }
     });
 
