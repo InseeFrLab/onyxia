6a784a065c224f8a14e3bf8df494e580397ea22f
Joseph Garrone <joseph.garrone.gj@gmail.com>
2024-08-28 17:29:23 +0200

    Add unit tests for listEnums

--- web/src/core/usecases/launcher/utils/shared/resolveEnum.test.ts
+++ web/src/core/usecases/launcher/utils/shared/resolveEnum.test.ts
@@ -26,4 +26,151 @@ describe(symToStr({ resolveEnum }), () => {
 
         expect(got).toStrictEqual(expected);
     });
+
+    it("approximates values from xOnyxiaContext", () => {
+        const xOnyxiaContext: XOnyxiaContextLike = {
+            user: {
+                decodedIdToken: {
+                    groups: ["1", "2", 3]
+                }
+            }
+        };
+
+        const got = resolveEnum({
+            "helmValuesSchema": {
+                "type": "number",
+                "x-onyxia": {
+                    "overwriteListEnumWith": "user.decodedIdToken.groups"
+                }
+            },
+            xOnyxiaContext
+        });
+
+        const expected = [1, 2, 3];
+
+        expect(got).toStrictEqual(expected);
+    });
+
+    it("works recursively", () => {
+        const xOnyxiaContext: XOnyxiaContextLike = {
+            "r": [
+                [1, 2, 3],
+                [4, "5", 6]
+            ]
+        };
+
+        const got = resolveEnum({
+            "helmValuesSchema": {
+                "type": "array",
+                "items": {
+                    "type": "number",
+                    "enum": [1, 2, 3, 4, 5, 6]
+                },
+                "x-onyxia": {
+                    "overwriteListEnumWith": "r"
+                }
+            },
+            xOnyxiaContext
+        });
+
+        const expected = [
+            [1, 2, 3],
+            [4, 5, 6]
+        ];
+
+        expect(got).toStrictEqual(expected);
+    });
+
+    it("falls back to listEnum", () => {
+        const xOnyxiaContext: XOnyxiaContextLike = {
+            "r": [
+                [1, 2, 3],
+                [4, "5", 6]
+            ]
+        };
+
+        const got = resolveEnum({
+            "helmValuesSchema": {
+                "type": "array",
+                "items": {
+                    "type": "number",
+                    "enum": [2, 3, 4, 5, 6]
+                },
+                "listEnum": [
+                    [2, 2, 2],
+                    [3, 3, 3]
+                ],
+                "x-onyxia": {
+                    "overwriteListEnumWith": "r"
+                }
+            },
+            xOnyxiaContext
+        });
+
+        const expected = [
+            [2, 2, 2],
+            [3, 3, 3]
+        ];
+
+        expect(got).toStrictEqual(expected);
+    });
+
+    it("fallback to enum", () => {
+        const xOnyxiaContext: XOnyxiaContextLike = {};
+
+        const got = resolveEnum({
+            "helmValuesSchema": {
+                "type": "string",
+                "listEnum": [1, 2, 3],
+                "enum": ["a", "b", "c"],
+                "x-onyxia": {
+                    "overwriteListEnumWith": "user.decodedIdToken.groups"
+                }
+            },
+            xOnyxiaContext
+        });
+
+        const expected = ["a", "b", "c"];
+
+        expect(got).toStrictEqual(expected);
+    });
+
+    it("do not crash is not render list and all invalid", () => {
+        const xOnyxiaContext: XOnyxiaContextLike = {};
+
+        const got = resolveEnum({
+            "helmValuesSchema": {
+                "type": "string",
+                "listEnum": [1, 2, 3],
+                "enum": ["a", "b", 3],
+                "x-onyxia": {
+                    "overwriteListEnumWith": "user.decodedIdToken.groups"
+                }
+            },
+            xOnyxiaContext
+        });
+
+        const expected = undefined;
+
+        expect(got).toStrictEqual(expected);
+    });
+
+    it("crash if no options and render is list", () => {
+        const xOnyxiaContext: XOnyxiaContextLike = {};
+
+        expect(() => {
+            resolveEnum({
+                "helmValuesSchema": {
+                    "type": "string",
+                    "render": "list",
+                    "listEnum": [1, 2, 3],
+                    "enum": ["a", "b", 3],
+                    "x-onyxia": {
+                        "overwriteListEnumWith": "user.decodedIdToken.groups"
+                    }
+                },
+                xOnyxiaContext
+            });
+        }).toThrowError();
+    });
 });
