b136d5c5c1b097d42e1170d37a13d78283366834
Joseph Garrone <joseph.garrone.gj@gmail.com>
2024-08-30 12:56:21 +0200

    Add unit test for getRootFrom

---
+++ web/src/core/usecases/launcher/utils/getRootForm/getRootForm.test.ts
@@ -0,0 +1,146 @@
+import { describe, it, expect } from "vitest";
+import { getRootForm } from "./getRootForm";
+import { symToStr } from "tsafe/symToStr";
+import type { RootForm } from "../../formTypes";
+
+describe(symToStr({ getRootForm }), () => {
+    it("simple case", () => {
+        const got = getRootForm({
+            "helmValuesSchema": {
+                "type": "object",
+                "properties": {
+                    "services": {
+                        "type": "object",
+                        "properties": {
+                            "a": { "type": "string" }
+                        }
+                    },
+                    "postgresql": {
+                        "type": "object",
+                        "properties": {
+                            "enabled": { "type": "boolean" }
+                        }
+                    },
+                    "global": {
+                        "type": "object",
+                        "properties": {
+                            "postgresql": {
+                                "type": "object",
+                                "properties": {
+                                    "username": { "type": "string" },
+                                    "password": { "type": "string" }
+                                }
+                            },
+                            "c": { "type": "number" }
+                        }
+                    }
+                }
+            },
+            "dependencies": [
+                {
+                    "chartName": "postgresql",
+                    "helmValuesPath_enabled": ["postgresql", "enabled"]
+                }
+            ],
+            "xOnyxiaContext": {},
+            "helmValues": {
+                "services": {
+                    "a": "foo"
+                },
+                "postgresql": {
+                    "enabled": true
+                },
+                "global": {
+                    "postgresql": {
+                        "username": "admin",
+                        "password": "xxx"
+                    },
+                    "c": 2
+                }
+            }
+        });
+
+        const expected: RootForm = {
+            "main": [
+                {
+                    "type": "group",
+                    "helmValuesPathSegment": "services",
+                    "description": undefined,
+                    "children": [
+                        {
+                            "type": "field",
+                            "title": "a",
+                            "isReadonly": false,
+                            "fieldType": "text field",
+                            "helmValuesPath": ["services", "a"],
+                            "description": undefined,
+                            "doRenderAsTextArea": false,
+                            "isSensitive": false,
+                            "pattern": undefined,
+                            "value": "foo"
+                        }
+                    ],
+                    "canAdd": false,
+                    "canRemove": false
+                }
+            ],
+            "disabledDependencies": [],
+            "global": [
+                {
+                    "type": "field",
+                    "title": "c",
+                    "isReadonly": false,
+                    "fieldType": "integer field",
+                    "helmValuesPath": ["global", "c"],
+                    "description": undefined,
+                    "value": 2,
+                    "isInteger": false,
+                    "minimum": undefined
+                }
+            ],
+            "dependencies": {
+                "postgresql": {
+                    "main": [
+                        {
+                            "type": "field",
+                            "title": "enabled",
+                            "isReadonly": false,
+                            "fieldType": "checkbox",
+                            "helmValuesPath": ["postgresql", "enabled"],
+                            "description": undefined,
+                            "value": true
+                        }
+                    ],
+                    "global": [
+                        {
+                            "type": "field",
+                            "title": "username",
+                            "isReadonly": false,
+                            "fieldType": "text field",
+                            "helmValuesPath": ["global", "postgresql", "username"],
+                            "description": undefined,
+                            "doRenderAsTextArea": false,
+                            "isSensitive": false,
+                            "pattern": undefined,
+                            "value": "admin"
+                        },
+                        {
+                            "type": "field",
+                            "title": "password",
+                            "isReadonly": false,
+                            "fieldType": "text field",
+                            "helmValuesPath": ["global", "postgresql", "password"],
+                            "description": undefined,
+                            "doRenderAsTextArea": false,
+                            "isSensitive": false,
+                            "pattern": undefined,
+                            "value": "xxx"
+                        }
+                    ]
+                }
+            }
+        };
+
+        expect(got).toStrictEqual(expected);
+    });
+});
