270ec6a5436df0ddd2de11cf0cecbfb93e99cfe2
Dylan Decrulle <dylan.decrulle@outlook.fr>
2024-09-10 07:22:31 +0200

    add i18n support in storybook

--- web/.storybook/preview.tsx
+++ web/.storybook/preview.tsx
@@ -1,12 +1,38 @@
 import type { Preview } from "@storybook/react";
 import { useDarkMode as useStorybookUiDarkMode } from "storybook-dark-mode";
 import { darkTheme, lightTheme } from "./theme";
-import React from "react";
+import React, { useEffect } from "react";
 import { OnyxiaUi } from "../src/ui/theme";
 import { injectCustomFontFaceIfNotAlreadyDone } from "../src/ui/theme/injectCustomFontFaceIfNotAlreadyDone";
+import {
+    I18nFetchingSuspense,
+    Language,
+    languages,
+    languagesPrettyPrint,
+    useLang,
+    fallbackLanguage
+} from "../src/ui/i18n";
+import { assert } from "tsafe/assert";
+import { is } from "tsafe/is";
 
 injectCustomFontFaceIfNotAlreadyDone();
 
+const languagesGlobal = Object.entries(languagesPrettyPrint).map(([value, title]) => ({
+    value: value as Language,
+    title
+}));
+
+const globalTypes = {
+    locale: {
+        name: "Locale",
+        description: "Internationalization locale",
+        defaultValue: fallbackLanguage,
+        toolbar: {
+            icon: "globe",
+            items: languagesGlobal
+        }
+    }
+};
 const preview: Preview = {
     parameters: {
         backgrounds: { disable: true },
@@ -16,15 +42,25 @@ const preview: Preview = {
         },
         docs: { disable: true, hidden: true }
     },
+    globalTypes,
     argTypes: {},
     decorators: [
-        (Story, {}) => {
+        (Story, { globals: { locale } }) => {
             const isStorybookUiDark = useStorybookUiDarkMode();
+            const { lang, setLang } = useLang();
+
+            assert(is<Language>(locale));
+
+            useEffect(() => {
+                if (lang !== locale) setLang(locale);
+            }, [locale]);
 
             return (
-                <OnyxiaUi darkMode={isStorybookUiDark}>
-                    <Story />
-                </OnyxiaUi>
+                <I18nFetchingSuspense>
+                    <OnyxiaUi darkMode={isStorybookUiDark}>
+                        <Story />
+                    </OnyxiaUi>
+                </I18nFetchingSuspense>
             );
         }
     ]
