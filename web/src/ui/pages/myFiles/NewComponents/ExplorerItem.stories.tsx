6624c8a64683bb0a10f66eced130ac06d83a2603
Dylan Decrulle <dylan.decrulle@outlook.fr>
2024-09-17 16:52:33 +0200

    wip

---
+++ web/src/ui/pages/myFiles/NewComponents/ExplorerItem.stories.tsx
@@ -0,0 +1,35 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { ExplorerItem } from "./ExplorerItem";
+import { action } from "@storybook/addon-actions";
+import { css } from "ui/theme";
+
+const meta = {
+    title: "Pages/MyFiles/NewComponents/ExplorerItem",
+    component: ExplorerItem
+} satisfies Meta<typeof ExplorerItem>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const FileSelected: Story = {
+    args: {
+        kind: "file",
+        basename: "example-file.txt",
+        size: 100000000,
+        className: css({ "width": "176px", "height": "176px" }),
+        isSelected: true,
+        onMouseEvent: action("Mouse event")
+    }
+};
+
+export const DirectoryUnselected: Story = {
+    args: {
+        kind: "directory",
+        basename: "example-directory",
+        size: 12345678901234,
+        className: css({ "width": "176px", "height": "176px" }),
+        isSelected: false,
+        onMouseEvent: action("Mouse event")
+    }
+};
