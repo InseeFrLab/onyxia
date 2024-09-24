526d541741c81712d54746780853f7a72b0ec556
Dylan Decrulle <dylan.decrulle@outlook.fr>
2024-09-24 10:14:22 +0200

    add new explorer items

--- web/src/ui/pages/myFiles/Explorer/ExplorerItems/ExplorerItem.stories.tsx
+++ web/src/ui/pages/myFiles/Explorer/ExplorerItems/ExplorerItem.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { ExplorerItem } from "./ExplorerItem";
 import { action } from "@storybook/addon-actions";
+import { css } from "ui/theme";
 
 const meta = {
     title: "Pages/MyFiles/Explorer/ExplorerItems/ExplorerItem",
@@ -15,9 +16,9 @@ export const FileSelected: Story = {
     args: {
         kind: "file",
         basename: "example-file.txt",
+        size: 100000000,
+        className: css({ "width": "160px", "height": "160px" }),
         isSelected: true,
-        isCircularProgressShown: false,
-        getIsValidBasename: ({ basename }) => basename.length > 0,
         onMouseEvent: action("Mouse event")
     }
 };
@@ -25,25 +26,10 @@ export const FileSelected: Story = {
 export const DirectoryUnselected: Story = {
     args: {
         kind: "directory",
-        basename: "example-folder",
+        basename: "example-directory",
+        size: 12345678901234,
+        className: css({ "width": "160px", "height": "160px" }),
         isSelected: false,
-        isCircularProgressShown: false,
-        getIsValidBasename: ({ basename }) => basename.length > 0,
         onMouseEvent: action("Mouse event")
-    }
-};
-
-export const FileEditing: Story = {
-    args: {
-        kind: "file",
-        basename: "new-file.txt",
-        isSelected: true,
-        isCircularProgressShown: false,
-        getIsValidBasename: ({ basename }) => basename.length > 0,
-        onMouseEvent: action("Mouse event")
-    },
-    play: async ({ canvasElement }) => {
-        const input = canvasElement.querySelector("input") as HTMLInputElement;
-        input && input.focus();
     }
 };
