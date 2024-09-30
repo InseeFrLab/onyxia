94e7647a744ae7c57100849ff1c7a8fb6df7eb27
Joseph Garrone <joseph.garrone.gj@gmail.com>
2024-09-30 22:58:55 +0200

    style FormFieldWrapper

--- web/src/ui/pages/launcher/formFields/shared/FormFieldWrapper.tsx
+++ web/src/ui/pages/launcher/formFields/shared/FormFieldWrapper.tsx
@@ -1,4 +1,8 @@
 import { tss } from "tss";
+import { Text } from "onyxia-ui/Text";
+import { IconButton } from "onyxia-ui/IconButton";
+import type { MuiIconComponentName } from "onyxia-ui/MuiIconComponentName";
+import { id } from "tsafe/id";
 
 type Props = {
     className?: string;
@@ -16,11 +20,20 @@ export function FormFieldWrapper(props: Props) {
 
     return (
         <div className={cx(classes.root, className)}>
-            <h3 lang="und">{title}</h3>
-            <button onClick={onResetToDefault}>Reset to default</button>
-            {description !== undefined && <p lang="und">{description}</p>}
-            {children}
-            {error !== undefined && error}
+            <div className={classes.header}>
+                <Text typo="object heading">{<span lang="und">{title}</span>}</Text>
+                <div style={{ "flex": 1 }} />
+                <IconButton
+                    onClick={onResetToDefault}
+                    icon={id<MuiIconComponentName>("SettingsBackupRestore")}
+                />
+            </div>
+            {description !== undefined && (
+                <Text typo="caption">{<span lang="und">{description}</span>}</Text>
+            )}
+
+            <div className={classes.childrenWrapper}>{children}</div>
+            <div className={classes.errorWrapper}>{error !== undefined && error}</div>
         </div>
     );
 }
@@ -33,5 +46,16 @@ const useStyles = tss
             "color": !isErrored
                 ? undefined
                 : theme.colors.useCases.alertSeverity.error.main
+        },
+        "header": {
+            "display": "flex",
+            "alignItems": "center"
+        },
+        "childrenWrapper": {
+            "marginTop": theme.spacing(4)
+        },
+        "errorWrapper": {
+            "marginTop": theme.spacing(3),
+            "minHeight": theme.typography.rootFontSizePx * 1.5
         }
     }));
