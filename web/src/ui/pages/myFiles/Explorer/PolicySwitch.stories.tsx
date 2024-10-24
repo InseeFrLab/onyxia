21665a30fd4c8061510a65c5c826d572c7077f51
Dylan Decrulle <dylan.decrulle@outlook.fr>
2024-10-24 17:12:08 +0200

    add policy switch

---
+++ web/src/ui/pages/myFiles/Explorer/PolicySwitch.stories.tsx
@@ -0,0 +1,44 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import {} from "./";
+import { action } from "@storybook/addon-actions";
+import { PolicySwitch } from "./PolicySwitch";
+import { useState } from "react";
+import { Item } from "../shared/types";
+
+const meta = {
+    title: "Pages/MyFiles/Explorer/PolicySwitch",
+    component: PolicySwitch,
+    argTypes: {
+        size: {
+            control: { type: "select" },
+            options: ["small", "medium", "large"]
+        },
+        policy: {
+            control: { type: "select" },
+            options: ["public", "private"]
+        }
+    }
+} satisfies Meta<typeof PolicySwitch>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+    args: {
+        changePolicy: action("Change policy"),
+        policy: "private"
+    },
+    render: props => {
+        const { changePolicy, ...rest } = props;
+        const [policy, setPolicy] = useState<Item["policy"]>(props.policy);
+
+        const handleChangePolicy = () => {
+            changePolicy();
+            setPolicy(prev => (prev === "private" ? "public" : "private"));
+        };
+        return (
+            <PolicySwitch {...rest} policy={policy} changePolicy={handleChangePolicy} />
+        );
+    }
+};
