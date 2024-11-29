0aa987c565c313391cc6670f5410a39b16f06c9a
Dylan Decrulle <dylan.decrulle@outlook.fr>
2024-11-29 15:56:29 +0100

    Delete ComponentType.ts

--- web/src/ui/tools/types/ComponentType.ts
+++
@@ -1,5 +0,0 @@
-import type { FC, ComponentClass } from "react";
-
-export type ComponentType<Props extends Record<string, unknown>> =
-    | ((props: Props) => ReturnType<FC>)
-    | ComponentClass<Props>;
