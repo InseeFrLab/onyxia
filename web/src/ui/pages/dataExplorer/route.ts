9b073108fdbed8af59a94aafeb1b14c6cb2d05de
Joseph Garrone <joseph.garrone.gj@gmail.com>
2024-12-06 12:57:47 +0100

    Code review

--- web/src/ui/pages/dataExplorer/route.ts
+++ web/src/ui/pages/dataExplorer/route.ts
@@ -2,30 +2,19 @@ import { createRouter, defineRoute, param, createGroup, type Route } from "type-
 import { id } from "tsafe/id";
 import type { ValueSerializer } from "type-route";
 
-export const DEFAULT_QUERY_PARAMS = {
-    source: "",
-    rowsPerPage: 25,
-    page: 1,
-    selectedRow: undefined,
-    columnVisibility: {}
-};
 export const routeDefs = {
     dataExplorer: defineRoute(
         {
             source: param.query.optional.string,
-            rowsPerPage: param.query.optional.number.default(
-                DEFAULT_QUERY_PARAMS.rowsPerPage
-            ),
-            page: param.query.optional.number.default(DEFAULT_QUERY_PARAMS.page),
+            rowsPerPage: param.query.optional.number,
+            page: param.query.optional.number,
             selectedRow: param.query.optional.number,
-            columnVisibility: param.query.optional
-                .ofType(
-                    id<ValueSerializer<Record<string, boolean>>>({
-                        parse: raw => JSON.parse(raw),
-                        stringify: value => JSON.stringify(value)
-                    })
-                )
-                .default(DEFAULT_QUERY_PARAMS.columnVisibility)
+            columnVisibility: param.query.optional.ofType(
+                id<ValueSerializer<Record<string, boolean>>>({
+                    parse: raw => JSON.parse(raw),
+                    stringify: value => JSON.stringify(value)
+                })
+            )
         },
         () => `/data-explorer`
     )
