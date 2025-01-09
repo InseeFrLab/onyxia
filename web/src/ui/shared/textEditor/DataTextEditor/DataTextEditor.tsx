4c41436287ba2b63177e43bf023172efc2df60f9
Joseph Garrone <joseph.garrone.gj@gmail.com>
2025-01-09 17:55:19 +0100

    Support YAML or JSON5

--- web/src/ui/shared/textEditor/DataTextEditor/DataTextEditor.tsx
+++ web/src/ui/shared/textEditor/DataTextEditor/DataTextEditor.tsx
@@ -1,4 +1,4 @@
-import { useState, useMemo } from "react";
+import { useState, useMemo, useEffect } from "react";
 import { TextEditor } from "../TextEditor";
 import { EditorState } from "@codemirror/state";
 import {
@@ -26,10 +26,11 @@ import {
     closeBrackets,
     closeBracketsKeymap
 } from "@codemirror/autocomplete";
-import { yamlSchema } from "codemirror-json-schema/yaml";
-//import type { JSONSchema7 } from "json-schema";
+import { json5Schema as codemirror_json_schema_json5 } from "codemirror-json-schema/json5";
+import { yamlSchema as codemirror_json_schema_yaml } from "codemirror-json-schema/yaml";
 import type { Stringifyable } from "core/tools/Stringifyable";
 import YAML from "yaml";
+import JSON5 from "json5";
 import { useConstCallback } from "powerhooks/useConstCallback";
 import { assert, type Equals } from "tsafe/assert";
 import memoize from "memoizee";
@@ -38,6 +39,10 @@ import Ajv from "ajv";
 import { Text } from "onyxia-ui/Text";
 import { tss } from "../tss";
 import { capitalize } from "tsafe/capitalize";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
 
 const ajv = new Ajv();
 
@@ -62,10 +67,22 @@ export type Props = {
     assert<Equals<Props, Props_Expected>>;
 }
 
-export default function DataTextEditor(props: Props) {
-    const { jsonSchema, value, onChange, ...rest } = props;
+function serializeValue(params: {
+    value: Stringifyable;
+    format: "YAML" | "JSON5";
+}): string {
+    const { value, format } = params;
 
-    const [valueStr, setValueStr] = useState(() => YAML.stringify(value));
+    switch (format) {
+        case "JSON5":
+            return JSON.stringify(value, null, 2);
+        case "YAML":
+            return YAML.stringify(value);
+    }
+}
+
+export default function DataTextEditor(props: Props) {
+    const { jsonSchema, value: value_default, onChange, ...rest } = props;
 
     const JSON_stringify_memo = useConst(() =>
         memoize((value: any) => JSON.stringify(value))
@@ -74,6 +91,15 @@ export default function DataTextEditor(props: Props) {
     const ajvValidateFunction = useMemo(
         () => ajv.compile(jsonSchema),
         [JSON_stringify_memo(jsonSchema)]
+    );
+
+    const [format, setFormat] = useState<"JSON5" | "YAML">("YAML");
+
+    const [valueStr, setValueStr] = useState<string>(() =>
+        serializeValue({
+            value: value_default,
+            format
+        })
     );
 
     const getErrorMsg = useConstCallback((value: Stringifyable) => {
@@ -91,36 +117,73 @@ export default function DataTextEditor(props: Props) {
         return error.message ?? "Invalid";
     });
 
-    const [errorMsg, setErrorMsg] = useState<string | undefined>(getErrorMsg(value));
+    const [errorMsg, setErrorMsg] = useState<string | undefined>(
+        getErrorMsg(value_default)
+    );
+
+    useEffect(() => {
+        setValueStr(
+            serializeValue({
+                value: value_default,
+                format
+            })
+        );
+        setErrorMsg(getErrorMsg(value_default));
+    }, [value_default]);
+
+    const onFormatChange = useConstCallback((newFormat: "JSON5" | "YAML") => {
+        const value = (() => {
+            switch (format) {
+                case "JSON5":
+                    return JSON5.parse(valueStr);
+                case "YAML":
+                    return YAML.parse(valueStr);
+            }
+        })();
+
+        setFormat(newFormat);
+        setValueStr(serializeValue({ value, format: newFormat }));
+    });
+
+    const extensions_base = useConst(() => [
+        //gutter({ class: "CodeMirror-lint-markers" }),
+        bracketMatching(),
+        highlightActiveLineGutter(),
+        // basicSetup,
+        closeBrackets(),
+        history(),
+        autocompletion(),
+        lineNumbers(),
+        lintGutter(),
+        indentOnInput(),
+        drawSelection(),
+        foldGutter(),
+        keymap.of([
+            ...closeBracketsKeymap,
+            ...defaultKeymap,
+            ...historyKeymap,
+            ...foldKeymap,
+            ...completionKeymap,
+            ...lintKeymap
+        ]),
+        EditorView.lineWrapping,
+        EditorState.tabSize.of(2),
+        syntaxHighlighting(oneDarkHighlightStyle)
+    ]);
 
     const extensions = useMemo(
         () => [
-            //gutter({ class: "CodeMirror-lint-markers" }),
-            bracketMatching(),
-            highlightActiveLineGutter(),
-            // basicSetup,
-            closeBrackets(),
-            history(),
-            autocompletion(),
-            lineNumbers(),
-            lintGutter(),
-            indentOnInput(),
-            drawSelection(),
-            foldGutter(),
-            keymap.of([
-                ...closeBracketsKeymap,
-                ...defaultKeymap,
-                ...historyKeymap,
-                ...foldKeymap,
-                ...completionKeymap,
-                ...lintKeymap
-            ]),
-            EditorView.lineWrapping,
-            EditorState.tabSize.of(2),
-            syntaxHighlighting(oneDarkHighlightStyle),
-            ...yamlSchema(jsonSchema)
+            ...extensions_base,
+            ...(() => {
+                switch (format) {
+                    case "JSON5":
+                        return codemirror_json_schema_json5(jsonSchema);
+                    case "YAML":
+                        return codemirror_json_schema_yaml(jsonSchema);
+                }
+            })()
         ],
-        [JSON_stringify_memo(jsonSchema)]
+        [format, JSON_stringify_memo(jsonSchema)]
     );
 
     const onChangeStr = useConstCallback((newValueStr: string) => {
@@ -128,11 +191,27 @@ export default function DataTextEditor(props: Props) {
 
         let newValue: Stringifyable;
 
-        try {
-            newValue = YAML.parse(newValueStr);
-        } catch {
-            setErrorMsg("Not a valid YAML string");
-            return;
+        switch (format) {
+            case "JSON5":
+                {
+                    try {
+                        newValue = JSON5.parse(newValueStr);
+                    } catch {
+                        setErrorMsg("Not a valid JSON5 string");
+                        return;
+                    }
+                }
+                break;
+            case "YAML":
+                {
+                    try {
+                        newValue = YAML.parse(newValueStr);
+                    } catch {
+                        setErrorMsg("Not a valid YAML string");
+                        return;
+                    }
+                }
+                break;
         }
 
         const errorMsg = getErrorMsg(newValue);
@@ -150,6 +229,20 @@ export default function DataTextEditor(props: Props) {
 
     return (
         <div className={classes.root}>
+            <FormControl fullWidth>
+                <InputLabel id="demo-simple-select-label">Format</InputLabel>
+                <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={format}
+                    label="Format"
+                    onChange={event => onFormatChange(event.target.value as any)}
+                >
+                    <MenuItem value={"YAML"}>YAML</MenuItem>
+                    <MenuItem value={"JSON5"}>JSON</MenuItem>
+                </Select>
+            </FormControl>
+
             <TextEditor
                 {...rest}
                 className={cx(classes.textEditor, rest.className)}
@@ -157,6 +250,7 @@ export default function DataTextEditor(props: Props) {
                 onChange={onChangeStr}
                 extensions={extensions}
             />
+
             {errorMsg !== undefined && (
                 <Text typo="body 2" className={classes.errorText}>
                     {capitalize(errorMsg)}
