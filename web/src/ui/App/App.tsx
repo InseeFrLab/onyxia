a221f5fc7366d146e9d1b6976021c00e6f584256
garronej <joseph.garrone@protonmail.com>
2025-03-11 21:15:27 +0100

    Use kc_locale if keycloak instead of ui_locales

--- web/src/ui/App/App.tsx
+++ web/src/ui/App/App.tsx
@@ -33,7 +33,7 @@ const { CoreProvider } = createCoreProvider({
     transformUrlBeforeRedirectToLogin: ({ authorizationUrl, isKeycloak }) => {
         authorizationUrl = addParamToUrl({
             url: authorizationUrl,
-            name: "ui_locales",
+            name: isKeycloak ? "kc_locale" : "ui_locales",
             value: evtLang.state
         }).newUrl;
 
