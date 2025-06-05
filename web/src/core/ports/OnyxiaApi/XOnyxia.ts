b3b386fd44baa7f2545a6f3eb0fd21c931d35b79
garronej <joseph.garrone@protonmail.com>
2025-06-05 18:48:01 +0200

    Add documentation link

--- web/src/core/ports/OnyxiaApi/XOnyxia.ts
+++ web/src/core/ports/OnyxiaApi/XOnyxia.ts
@@ -82,6 +82,7 @@ export type XOnyxiaContext = {
         decodedIdToken: Record<string, unknown>;
         accessToken: string;
         refreshToken: string;
+        // See: https://docs.onyxia.sh/v/v10/admin-doc/catalog-of-services/customize-your-charts/declarative-user-profile
         profile: Record<string, Stringifyable> | undefined;
     };
     service: {
