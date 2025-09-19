7a7a715408b74ffdde744dc7f9029fc22d68aa12
garronej <joseph.garrone@protonmail.com>
2025-09-19 17:09:51 +0200

    Don't put too much logic in a file that cannot be re-evaluated during developement

--- web/src/main.lazy.tsx
+++ web/src/main.lazy.tsx
@@ -1,16 +1,6 @@
-/* eslint-disable react-refresh/only-export-components */
-import { Suspense, lazy, StrictMode, useEffect } from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import {
-    OnyxiaUi,
-    loadThemedFavicon,
-    injectCustomFontFaceIfNotAlreadyDone
-} from "ui/theme";
-import { useSplashScreen } from "onyxia-ui";
-
-loadThemedFavicon();
-// NOTE: We do that only to showcase the app with an other font with the URL.
-injectCustomFontFaceIfNotAlreadyDone();
+import { Root } from "./Root";
 
 {
     const version = import.meta.env.WEB_VERSION;
@@ -23,40 +13,8 @@ injectCustomFontFaceIfNotAlreadyDone();
     );
 }
 
-const App = lazy(() => import("ui/App"));
-
-function Root() {
-    const { hideRootSplashScreen } = useSplashScreen();
-
-    useEffect(() => {
-        hideRootSplashScreen();
-    }, []);
-
-    return (
-        <Suspense fallback={<SplashScreen />}>
-            <App />
-        </Suspense>
-    );
-}
-
-function SplashScreen() {
-    const { showSplashScreen, hideSplashScreen } = useSplashScreen();
-
-    useEffect(() => {
-        showSplashScreen({ enableTransparency: false });
-
-        return () => {
-            hideSplashScreen();
-        };
-    }, []);
-
-    return null;
-}
-
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <OnyxiaUi>
-            <Root />
-        </OnyxiaUi>
+        <Root />
     </StrictMode>
 );
