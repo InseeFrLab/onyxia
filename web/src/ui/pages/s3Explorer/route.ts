2374970f0beba36114cd7f162321d2498bc814f0
garronej <joseph.garrone@protonmail.com>
2025-10-24 18:18:24 +0200

    Use special character to represent slash

--- web/src/ui/pages/s3Explorer/route.ts
+++ web/src/ui/pages/s3Explorer/route.ts
@@ -1,10 +1,17 @@
 import { defineRoute, createGroup, param } from "type-route";
+import { type ValueSerializer } from "type-route";
+import { id } from "tsafe";
 
 export const routeDefs = {
     s3Explorer: defineRoute(
         {
             bucket: param.query.optional.string,
-            prefix: param.query.optional.string,
+            prefix: param.query.optional.ofType(
+                id<ValueSerializer<string>>({
+                    parse: raw => raw.replace(/\u2044/g, "/"),
+                    stringify: value => value.replace(/\//g, "\u2044")
+                })
+            ),
             profile: param.query.optional.string
         },
         () => `/s3`
