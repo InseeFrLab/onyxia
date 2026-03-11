a7d7cce0a7187dead9f4d9a90762069beef9aa73
garronej <joseph.garrone@protonmail.com>
2026-03-11 14:16:56 +0100

    Changes propagation to UI

--- web/src/ui/pages/s3Explorer/route.ts
+++ web/src/ui/pages/s3Explorer/route.ts
@@ -3,17 +3,17 @@ import { defineRoute, createGroup, param } from "type-route";
 export const routeDefs = {
     s3Explorer: defineRoute(
         {
-            s3UriPrefixWithoutScheme: param.path.trailing.ofType({
+            s3UriWithoutScheme: param.path.trailing.ofType({
                 parse: raw => decodeURIComponent(raw), // decode the path
                 stringify: value => encodeURI(value) // encode when creating URL
             }),
             profile: param.query.optional.string
         },
-        ({ s3UriPrefixWithoutScheme }) => `/s3/${s3UriPrefixWithoutScheme}`
+        ({ s3UriWithoutScheme }) => `/s3/${s3UriWithoutScheme}`
     ),
     s3Explorer_root: defineRoute(
         {
-            s3UriPrefixWithoutScheme: param.query.optional.string.default(""),
+            s3UriWithoutScheme: param.query.optional.string.default(""),
             profile: param.query.optional.string
         },
         () => "/s3"
