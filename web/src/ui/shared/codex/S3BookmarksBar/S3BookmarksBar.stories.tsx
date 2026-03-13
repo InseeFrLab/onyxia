a3cbb3e063ecf4c690ef82774a66ac89faea5c87
garronej <joseph.garrone@protonmail.com>
2026-03-13 13:35:37 +0100

    Fix compilation error

--- web/src/ui/shared/codex/S3BookmarksBar/S3BookmarksBar.stories.tsx
+++ web/src/ui/shared/codex/S3BookmarksBar/S3BookmarksBar.stories.tsx
@@ -2,6 +2,7 @@ import type { Meta, StoryObj } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 import { parseS3Uri, stringifyS3Uri, type S3Uri } from "core/tools/S3Uri";
 import { S3BookmarksBar, type S3BookmarksBarProps } from "./S3BookmarksBar";
+import { assert } from "tsafe";
 
 const meta = {
     title: "Shared/S3BookmarksBar",
@@ -12,12 +13,13 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-function parsePrefixOrThrow(s3Uri: string): S3Uri.Prefix {
-    return parseS3Uri({
+function parsePrefixOrThrow(s3Uri: string): S3Uri.TerminatedByDelimiter {
+    const obj = parseS3Uri({
         value: s3Uri,
-        delimiter: "/",
-        isPrefix: true
+        delimiter: "/"
     });
+    assert(obj.isDelimiterTerminated);
+    return obj;
 }
 
 const baseItems: S3BookmarksBarProps["items"] = [
