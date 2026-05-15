f715784fb5f7ed5cc61384d4e4d163026db16f98
garronej <joseph.garrone@protonmail.com>
2026-05-15 17:46:14 +0200

    Minor refactor

--- web/src/core/usecases/projectManagement/selectors.ts
+++ web/src/core/usecases/projectManagement/selectors.ts
@@ -49,8 +49,8 @@ export const selectors = {
     ),
     canInjectPersonalInfos: createSelector(
         protectedSelectors.currentProject,
-        currentProject => {
-            return currentProject.doInjectPersonalInfos;
-        }
+        createSelector(state, state => state.disablePersonalInfosInjectionInGroup),
+        (currentProject, disablePersonalInfosInjectionInGroup) =>
+            currentProject.group === undefined || !disablePersonalInfosInjectionInGroup
     )
 };
